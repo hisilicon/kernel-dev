ccef4f4bba752c2c7602d41a5dbb1e25e0dec39f
Ma Jun <majun258@huawei.com>
2016-02-23 18:53:10 +0800

    {topost} Irq/mbigen:Promote the mbigen driver register timing
    Using module_platform_driver() to register mbigen driver is
    too late for some driver to apply irq, because the mbigen irq
    domain is not created yet.
    
    Signed-off-by: Ma Jun <majun258@huawei.com>
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>

--- drivers/irqchip/irq-mbigen.c
+++ drivers/irqchip/irq-mbigen.c
@@ -299,7 +299,14 @@ static struct platform_driver mbigen_platform_driver = {
 	.probe			= mbigen_device_probe,
 };
 
-module_platform_driver(mbigen_platform_driver);
+static __init int mbigen_init(void)
+
+{
+	return platform_driver_register(&mbigen_platform_driver);
+
+}
+
+arch_initcall(mbigen_init);
 
 MODULE_AUTHOR("Jun Ma <majun258@huawei.com>");
 MODULE_AUTHOR("Yun Wu <wuyun.wu@huawei.com>");
