6ec199cee1b1500236cd7244233c2e8151eb1e35
Anurup M <anurup.m@huawei.com>
2016-11-28 00:29:05 -0500

    {fromlist v2} perf: hisi: separate read and write function and other fixes.
    1. Separate read and write functions.
    2. Add helper functions for djtag prepare and operation.
    3. Change compatible names to use hisi- prefix.
    4. Remove EXPORT_SYMBOL as now djtag is a part of PMU driver.
    5. Use of_node_get and of_node_put so as to retain reference.
    
    Signed-off-by: Anurup M <anurup.m@huawei.com>
    Reviewed-by: John Garry <john.garry@huawei.com>

--- drivers/perf/hisilicon/djtag.c
+++ drivers/perf/hisilicon/djtag.c
@@ -9,6 +9,7 @@
  */
 
 #include <linux/bitops.h>
+#include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/list.h>
 #include <linux/module.h>
@@ -18,11 +19,11 @@
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
+#include <linux/time64.h>
 
-#include <asm-generic/delay.h>
 #include "djtag.h"
 
-#define SC_DJTAG_TIMEOUT		100000	/* 100ms */
+#define SC_DJTAG_TIMEOUT_US    (100 * USEC_PER_MSEC) /* 100ms */
 
 /* for djtag v1 */
 #define SC_DJTAG_MSTR_EN		0x6800
@@ -59,18 +60,25 @@
 
 #define DJTAG_PREFIX "hisi-djtag-dev-"
 
-DEFINE_IDR(djtag_hosts_idr);
+static DEFINE_IDR(djtag_hosts_idr);
+
+struct hisi_djtag_ops {
+	int (*djtag_read)(void __iomem *regs_base, u32 offset,
+					u32 mod_sel, u32 mod_mask,
+					int chain_id, u32 *rval);
+	int (*djtag_write)(void __iomem *regs_base, u32 offset,
+					u32 mod_sel, u32 mod_mask,
+					u32 wval, int chain_id);
+};
 
 struct hisi_djtag_host {
 	spinlock_t lock;
-	int dev_id;
+	int id;
 	struct device dev;
 	struct list_head client_list;
 	void __iomem *sysctl_reg_map;
 	struct device_node *of_node;
-	int (*djtag_readwrite)(void __iomem *regs_base, u32 offset,
-			u32 mod_sel, u32 mod_mask, bool is_w,
-			u32 wval, int chain_id, u32 *rval);
+	const struct hisi_djtag_ops *djtag_ops;
 };
 
 #define to_hisi_djtag_client(d) container_of(d, struct hisi_djtag_client, dev)
@@ -78,120 +86,65 @@ struct hisi_djtag_host {
 								 driver)
 #define MODULE_PREFIX "hisi_djtag:"
 
-static void djtag_read32_relaxed(void __iomem *regs_base, u32 off, u32 *value)
+static void djtag_prepare_v1(void __iomem *regs_base, u32 offset,
+						u32 mod_sel, u32 mod_mask)
 {
-	void __iomem *reg_addr = regs_base + off;
-
-	*value = readl_relaxed(reg_addr);
-}
-
-static void djtag_write32(void __iomem *regs_base, u32 off, u32 val)
-{
-	void __iomem *reg_addr = regs_base + off;
-
-	writel(val, reg_addr);
-}
-
-/*
- * djtag_readwrite_v1/v2: djtag read/write interface
- * @reg_base:	djtag register base address
- * @offset:	register's offset
- * @mod_sel:	module selection
- * @mod_mask:	mask to select specific modules for write
- * @is_w:	write -> true, read -> false
- * @wval:	value to register for write
- * @chain_id:	which sub module for read
- * @rval:	value in register for read
- *
- * Return non-zero if error, else return 0.
- */
-static int djtag_readwrite_v1(void __iomem *regs_base, u32 offset, u32 mod_sel,
-		u32 mod_mask, bool is_w, u32 wval, int chain_id, u32 *rval)
-{
-	u32 rd;
-	int timeout = SC_DJTAG_TIMEOUT;
-
-	if (!(mod_mask & CHAIN_UNIT_CFG_EN)) {
-		pr_warn("djtag: do nothing.\n");
-		return 0;
-	}
-
-	/* djtag mster enable & accelerate R,W */
-	djtag_write32(regs_base, SC_DJTAG_MSTR_EN,
-			DJTAG_NOR_CFG | DJTAG_MSTR_EN);
+	/* djtag master enable & accelerate R,W */
+	writel(DJTAG_NOR_CFG | DJTAG_MSTR_EN, regs_base + SC_DJTAG_MSTR_EN);
 
 	/* select module */
-	djtag_write32(regs_base, SC_DJTAG_DEBUG_MODULE_SEL, mod_sel);
-	djtag_write32(regs_base, SC_DJTAG_CHAIN_UNIT_CFG_EN,
-				mod_mask & CHAIN_UNIT_CFG_EN);
-
-	if (is_w) {
-		djtag_write32(regs_base, SC_DJTAG_MSTR_WR, DJTAG_MSTR_W);
-		djtag_write32(regs_base, SC_DJTAG_MSTR_DATA, wval);
-	} else
-		djtag_write32(regs_base, SC_DJTAG_MSTR_WR, DJTAG_MSTR_R);
+	writel(mod_sel, regs_base + SC_DJTAG_DEBUG_MODULE_SEL);
+	writel(mod_mask & CHAIN_UNIT_CFG_EN,
+			regs_base + SC_DJTAG_CHAIN_UNIT_CFG_EN);
 
 	/* address offset */
-	djtag_write32(regs_base, SC_DJTAG_MSTR_ADDR, offset);
+	writel(offset, regs_base + SC_DJTAG_MSTR_ADDR);
+}
+
+static int djtag_do_operation_v1(void __iomem *regs_base)
+{
+	u32 rd;
+	int timeout = SC_DJTAG_TIMEOUT_US;
 
 	/* start to write to djtag register */
-	djtag_write32(regs_base, SC_DJTAG_MSTR_START_EN, DJTAG_MSTR_START_EN);
+	writel(DJTAG_MSTR_START_EN, regs_base + SC_DJTAG_MSTR_START_EN);
 
 	/* ensure the djtag operation is done */
 	do {
-		djtag_read32_relaxed(regs_base, SC_DJTAG_MSTR_START_EN, &rd);
+		rd = readl(regs_base + SC_DJTAG_MSTR_START_EN);
 		if (!(rd & DJTAG_MSTR_EN))
 			break;
 
 		udelay(1);
 	} while (timeout--);
 
-	if (timeout < 0) {
-		pr_err("djtag: %s timeout!\n", is_w ? "write" : "read");
+	if (timeout < 0)
 		return -EBUSY;
-	}
-
-	if (!is_w)
-		djtag_read32_relaxed(regs_base,
-			SC_DJTAG_RD_DATA_BASE + chain_id * 0x4, rval);
 
 	return 0;
 }
 
-static int djtag_readwrite_v2(void __iomem *regs_base, u32 offset, u32 mod_sel,
-		u32 mod_mask, bool is_w, u32 wval, int chain_id, u32 *rval)
+static void djtag_prepare_v2(void __iomem *regs_base, u32 offset,
+						u32 mod_sel, u32 mod_mask)
 {
-	u32 rd;
-	int timeout = SC_DJTAG_TIMEOUT;
-
-	if (!(mod_mask & CHAIN_UNIT_CFG_EN_EX)) {
-		pr_warn("djtag: do nothing.\n");
-		return 0;
-	}
-
 	/* djtag mster enable */
-	djtag_write32(regs_base, SC_DJTAG_SEC_ACC_EN_EX, DJTAG_SEC_ACC_EN_EX);
-
-	if (is_w) {
-		djtag_write32(regs_base, SC_DJTAG_MSTR_CFG_EX, DJTAG_MSTR_WR_EX
-				| (mod_sel << DEBUG_MODULE_SEL_SHIFT_EX)
-				| (mod_mask & CHAIN_UNIT_CFG_EN_EX));
-		djtag_write32(regs_base, SC_DJTAG_MSTR_DATA_EX, wval);
-	} else
-		djtag_write32(regs_base, SC_DJTAG_MSTR_CFG_EX, DJTAG_MSTR_RD_EX
-				| (mod_sel << DEBUG_MODULE_SEL_SHIFT_EX)
-				| (mod_mask & CHAIN_UNIT_CFG_EN_EX));
+	writel(DJTAG_SEC_ACC_EN_EX, regs_base + SC_DJTAG_SEC_ACC_EN_EX);
 
 	/* address offset */
-	djtag_write32(regs_base, SC_DJTAG_MSTR_ADDR_EX, offset);
+	writel(offset, regs_base + SC_DJTAG_MSTR_ADDR_EX);
+}
+
+static int djtag_do_operation_v2(void __iomem *regs_base)
+{
+	u32 rd;
+	int timeout = SC_DJTAG_TIMEOUT_US;
 
 	/* start to write to djtag register */
-	djtag_write32(regs_base,
-		      SC_DJTAG_MSTR_START_EN_EX, DJTAG_MSTR_START_EN_EX);
+	writel(DJTAG_MSTR_START_EN_EX, regs_base + SC_DJTAG_MSTR_START_EN_EX);
 
 	/* ensure the djtag operation is done */
 	do {
-		djtag_read32_relaxed(regs_base, SC_DJTAG_MSTR_START_EN_EX, &rd);
+		rd = readl(regs_base + SC_DJTAG_MSTR_START_EN_EX);
 
 		if (!(rd & DJTAG_MSTR_START_EN_EX))
 			break;
@@ -202,9 +155,9 @@ static int djtag_readwrite_v2(void __iomem *regs_base, u32 offset, u32 mod_sel,
 	if (timeout < 0)
 		goto timeout;
 
-	timeout = SC_DJTAG_TIMEOUT;
+	timeout = SC_DJTAG_TIMEOUT_US;
 	do {
-		djtag_read32_relaxed(regs_base, SC_DJTAG_OP_ST_EX, &rd);
+		rd = readl(regs_base + SC_DJTAG_OP_ST_EX);
 
 		if (rd & DJTAG_OP_DONE_EX)
 			break;
@@ -215,22 +168,146 @@ static int djtag_readwrite_v2(void __iomem *regs_base, u32 offset, u32 mod_sel,
 	if (timeout < 0)
 		goto timeout;
 
-	if (!is_w)
-		djtag_read32_relaxed(regs_base,
-				     SC_DJTAG_RD_DATA_BASE_EX + chain_id * 0x4,
-									rval);
-
 	return 0;
 
 timeout:
-	pr_err("djtag: %s timeout!\n", is_w ? "write" : "read");
 	return -EBUSY;
 }
 
+/*
+ * djtag_read_v1/v2: djtag read interface
+ * @reg_base:	djtag register base address
+ * @offset:	register's offset
+ * @mod_sel:	module selection
+ * @mod_mask:	mask to select specific modules for write
+ * @chain_id:	which sub module for read
+ * @rval:	value in register for read
+ *
+ * Return non-zero if error, else return 0.
+ */
+static int djtag_read_v1(void __iomem *regs_base, u32 offset, u32 mod_sel,
+					u32 mod_mask, int chain_id, u32 *rval)
+{
+	int ret;
+
+	if (!(mod_mask & CHAIN_UNIT_CFG_EN)) {
+		pr_warn("djtag: do nothing.\n");
+		return 0;
+	}
+
+	djtag_prepare_v1(regs_base, offset, mod_sel, mod_mask);
+
+	writel(DJTAG_MSTR_R, regs_base + SC_DJTAG_MSTR_WR);
+
+	ret = djtag_do_operation_v1(regs_base);
+	if (ret) {
+		if (ret == EBUSY)
+			pr_err("djtag: %s timeout!\n", "read");
+		return ret;
+	}
+
+	*rval = readl(regs_base + SC_DJTAG_RD_DATA_BASE + chain_id * 0x4);
+
+	return 0;
+}
+
+static int djtag_read_v2(void __iomem *regs_base, u32 offset, u32 mod_sel,
+					u32 mod_mask, int chain_id, u32 *rval)
+{
+	int ret;
+
+	if (!(mod_mask & CHAIN_UNIT_CFG_EN_EX)) {
+		pr_warn("djtag: do nothing.\n");
+		return 0;
+	}
+
+	djtag_prepare_v2(regs_base, offset, mod_sel, mod_mask);
+
+	writel(DJTAG_MSTR_RD_EX
+			| (mod_sel << DEBUG_MODULE_SEL_SHIFT_EX)
+			| (mod_mask & CHAIN_UNIT_CFG_EN_EX),
+				regs_base + SC_DJTAG_MSTR_CFG_EX);
+
+	ret = djtag_do_operation_v2(regs_base);
+	if (ret) {
+		if (ret == EBUSY)
+			pr_err("djtag: %s timeout!\n", "read");
+		return ret;
+	}
+
+	*rval = readl(regs_base + SC_DJTAG_RD_DATA_BASE_EX +
+					      chain_id * 0x4);
+
+	return 0;
+}
+
+/*
+ * djtag_write_v1/v2: djtag write interface
+ * @reg_base:	djtag register base address
+ * @offset:	register's offset
+ * @mod_sel:	module selection
+ * @mod_mask:	mask to select specific modules for write
+ * @wval:	value to register for write
+ * @chain_id:	which sub module for read
+ *
+ * Return non-zero if error, else return 0.
+ */
+static int djtag_write_v1(void __iomem *regs_base, u32 offset, u32 mod_sel,
+					u32 mod_mask, u32 wval, int chain_id)
+{
+	int ret;
+
+	if (!(mod_mask & CHAIN_UNIT_CFG_EN)) {
+		pr_warn("djtag: do nothing.\n");
+		return 0;
+	}
+
+	djtag_prepare_v1(regs_base, offset, mod_sel, mod_mask);
+
+	writel(DJTAG_MSTR_W, regs_base + SC_DJTAG_MSTR_WR);
+	writel(wval, regs_base + SC_DJTAG_MSTR_DATA);
+
+	ret = djtag_do_operation_v1(regs_base);
+	if (ret) {
+		if (ret == EBUSY)
+			pr_err("djtag: %s timeout!\n", "write");
+		return ret;
+	}
+
+	return 0;
+}
+
+static int djtag_write_v2(void __iomem *regs_base, u32 offset, u32 mod_sel,
+					u32 mod_mask, u32 wval, int chain_id)
+{
+	int ret;
+
+	if (!(mod_mask & CHAIN_UNIT_CFG_EN_EX)) {
+		pr_warn("djtag: do nothing.\n");
+		return 0;
+	}
+
+	djtag_prepare_v2(regs_base, offset, mod_sel, mod_mask);
+
+	writel(DJTAG_MSTR_WR_EX
+			| (mod_sel << DEBUG_MODULE_SEL_SHIFT_EX)
+			| (mod_mask & CHAIN_UNIT_CFG_EN_EX),
+				regs_base + SC_DJTAG_MSTR_CFG_EX);
+	writel(wval, regs_base + SC_DJTAG_MSTR_DATA_EX);
+
+	ret = djtag_do_operation_v2(regs_base);
+	if (ret) {
+		if (ret == EBUSY)
+			pr_err("djtag: %s timeout!\n", "write");
+		return ret;
+	}
+
+	return 0;
+}
 
 /**
  * djtag_writel - write registers via djtag
- * @node:	djtag node
+ * @client: djtag client handle
  * @offset:	register's offset
  * @mod_sel:	module selection
  * @mod_mask:	mask to select specific modules
@@ -238,27 +315,26 @@ timeout:
  *
  * If error return errno, otherwise return 0.
  */
-int hisi_djtag_writel(struct hisi_djtag_client *client, u32 offset, u32 mod_sel,
-			u32 mod_mask, u32 val)
+int hisi_djtag_writel(struct hisi_djtag_client *client, u32 offset,
+				 u32 mod_sel, u32 mod_mask, u32 val)
 {
 	void __iomem *reg_map = client->host->sysctl_reg_map;
 	unsigned long flags;
 	int ret = 0;
 
 	spin_lock_irqsave(&client->host->lock, flags);
-	ret = client->host->djtag_readwrite(reg_map, offset, mod_sel, mod_mask,
-					true, val, 0, NULL);
+	ret = client->host->djtag_ops->djtag_write(reg_map, offset, mod_sel,
+						mod_mask, val, 0);
 	if (ret)
 		pr_err("djtag_writel: error! ret=%d\n", ret);
 	spin_unlock_irqrestore(&client->host->lock, flags);
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(hisi_djtag_writel);
 
 /**
  * djtag_readl - read registers via djtag
- * @node:	djtag node
+ * @client: djtag client handle
  * @offset:	register's offset
  * @mod_sel:	module type selection
  * @chain_id:	chain_id number, mostly is 0
@@ -266,46 +342,42 @@ EXPORT_SYMBOL_GPL(hisi_djtag_writel);
  *
  * If error return errno, otherwise return 0.
  */
-int hisi_djtag_readl(struct hisi_djtag_client *client, u32 offset, u32 mod_sel,
-		int chain_id, u32 *val)
+int hisi_djtag_readl(struct hisi_djtag_client *client, u32 offset,
+				u32 mod_sel, int chain_id, u32 *val)
 {
 	void __iomem *reg_map = client->host->sysctl_reg_map;
 	unsigned long flags;
 	int ret = 0;
 
 	spin_lock_irqsave(&client->host->lock, flags);
-	ret = client->host->djtag_readwrite(reg_map, offset, mod_sel,
-			0xffff, false, 0, chain_id, val);
+	ret = client->host->djtag_ops->djtag_read(reg_map, offset, mod_sel,
+					0xffff, chain_id, val);
 	if (ret)
 		pr_err("djtag_readl: error! ret=%d\n", ret);
 	spin_unlock_irqrestore(&client->host->lock, flags);
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(hisi_djtag_readl);
 
-static const struct of_device_id djtag_of_match[] = {
-	/* for hip05(D02) cpu die */
-	{ .compatible = "hisilicon,hip05-cpu-djtag-v1",
-		.data = (void *)djtag_readwrite_v1 },
-	/* for hip05(D02) io die */
-	{ .compatible = "hisilicon,hip05-io-djtag-v1",
-		.data = (void *)djtag_readwrite_v1 },
-	/* for hip06(D03) cpu die */
-	{ .compatible = "hisilicon,hip06-cpu-djtag-v1",
-		.data = (void *)djtag_readwrite_v1 },
-	/* for hip06(D03) io die */
-	{ .compatible = "hisilicon,hip06-io-djtag-v2",
-		.data = (void *)djtag_readwrite_v2 },
-	/* for hip07(D05) cpu die */
-	{ .compatible = "hisilicon,hip07-cpu-djtag-v2",
-		.data = (void *)djtag_readwrite_v2 },
-	/* for hip07(D05) io die */
-	{ .compatible = "hisilicon,hip07-io-djtag-v2",
-		.data = (void *)djtag_readwrite_v2 },
-	{},
+static const struct hisi_djtag_ops djtag_v1_ops = {
+	.djtag_read  = djtag_read_v1,
+	.djtag_write  = djtag_write_v1,
 };
 
+static const struct hisi_djtag_ops djtag_v2_ops = {
+	.djtag_read  = djtag_read_v2,
+	.djtag_write  = djtag_write_v2,
+};
+
+static const struct of_device_id djtag_of_match[] = {
+	/* for hip05/06 CPU die, hip05 IO die */
+	{ .compatible = "hisilicon,hisi-djtag-v1",
+		.data = &djtag_v1_ops },
+	/* for hip07 CPU die, hip06/07 IO die */
+	{ .compatible = "hisilicon,hisi-djtag-v2",
+		.data = &djtag_v2_ops },
+	{},
+};
 MODULE_DEVICE_TABLE(of, djtag_of_match);
 
 static ssize_t
@@ -315,7 +387,7 @@ show_modalias(struct device *dev, struct device_attribute *attr, char *buf)
 
 	return sprintf(buf, "%s%s\n", MODULE_PREFIX, client->name);
 }
-static DEVICE_ATTR(modalias, S_IRUGO, show_modalias, NULL);
+static DEVICE_ATTR(modalias, 0444, show_modalias, NULL);
 
 static struct attribute *hisi_djtag_dev_attrs[] = {
 	NULL,
@@ -393,7 +465,6 @@ static int hisi_djtag_device_remove(struct device *dev)
 static int hisi_djtag_device_match(struct device *dev,
 					struct device_driver *drv)
 {
-	const struct of_device_id *p;
 	struct hisi_djtag_client *client = hisi_djtag_verify_client(dev);
 
 	if (!client)
@@ -402,11 +473,7 @@ static int hisi_djtag_device_match(struct device *dev,
 	if (of_driver_match_device(dev, drv))
 		return true;
 
-	p = of_match_device(drv->of_match_table, dev);
-	if (!p)
-		return false;
-
-	return true;
+	return false;
 }
 
 struct bus_type hisi_djtag_bus = {
@@ -416,11 +483,9 @@ struct bus_type hisi_djtag_bus = {
 	.remove		= hisi_djtag_device_remove,
 };
 
-struct hisi_djtag_client *hisi_djtag_new_device(struct hisi_djtag_host *host,
-						struct device_node *node)
+struct hisi_djtag_client *hisi_djtag_client_alloc(struct hisi_djtag_host *host)
 {
 	struct hisi_djtag_client *client;
-	int status;
 
 	client = kzalloc(sizeof(*client), GFP_KERNEL);
 	if (!client)
@@ -431,51 +496,46 @@ struct hisi_djtag_client *hisi_djtag_new_device(struct hisi_djtag_host *host,
 	client->dev.parent = &client->host->dev;
 	client->dev.bus = &hisi_djtag_bus;
 	client->dev.type = &hisi_djtag_client_type;
-	client->dev.of_node = node;
-	snprintf(client->name, DJTAG_CLIENT_NAME_LEN, "%s%s",
-					DJTAG_PREFIX, node->name);
-	dev_set_name(&client->dev, "%s", client->name);
-
-	status = device_register(&client->dev);
-	if (status < 0) {
-		pr_err("error adding new device, status=%d\n", status);
-		kfree(client);
-		return NULL;
-	}
 
 	return client;
 }
 
-static struct hisi_djtag_client *hisi_djtag_of_register_device(
-						struct hisi_djtag_host *host,
+static int hisi_djtag_new_of_device(struct hisi_djtag_host *host,
 						struct device_node *node)
 {
 	struct hisi_djtag_client *client;
+	int rc;
 
-	client = hisi_djtag_new_device(host, node);
-	if (client == NULL) {
-		dev_err(&host->dev, "error registering device %s\n",
-			node->full_name);
-		of_node_put(node);
-		return ERR_PTR(-EINVAL);
+	client = hisi_djtag_client_alloc(host);
+	if (!client)
+		return -ENOMEM;
+
+	client->dev.of_node = of_node_get(node);
+	snprintf(client->name, DJTAG_CLIENT_NAME_LEN, "%s%s",
+					DJTAG_PREFIX, node->name);
+	dev_set_name(&client->dev, "%s", client->name);
+
+	rc = device_register(&client->dev);
+	if (rc < 0) {
+		pr_err("DT: error adding new device, rc=%d\n", rc);
+		kfree(client);
+		return -EINVAL;
 	}
 
-	return client;
+	list_add(&client->next, &host->client_list);
+	return 0;
 }
 
 static void djtag_register_devices(struct hisi_djtag_host *host)
 {
 	struct device_node *node;
-	struct hisi_djtag_client *client;
 
-	if (!host->of_node)
-		return;
-
-	for_each_available_child_of_node(host->of_node, node) {
-		if (of_node_test_and_set_flag(node, OF_POPULATED))
-			continue;
-		client = hisi_djtag_of_register_device(host, node);
-		list_add(&client->next, &host->client_list);
+	if (host->of_node) {
+		for_each_available_child_of_node(host->of_node, node) {
+			if (of_node_test_and_set_flag(node, OF_POPULATED))
+				continue;
+			hisi_djtag_new_of_device(host, node);
+		}
 	}
 }
 
@@ -486,19 +546,19 @@ static int hisi_djtag_add_host(struct hisi_djtag_host *host)
 	host->dev.bus = &hisi_djtag_bus;
 
 	rc = idr_alloc(&djtag_hosts_idr, host, 0, 0, GFP_KERNEL);
-        if (rc < 0) {
-                dev_err(&host->dev, "No available djtag host ID'!s\n");
-                return rc;
-        }
-        host->dev_id = rc;
+	if (rc < 0) {
+		dev_err(&host->dev, "No available djtag host ID'!s\n");
+		return rc;
+	}
+	host->id = rc;
 
 	/* Suffix the unique ID and set djtag hostname */
-	dev_set_name(&host->dev, "djtag-host-%d", host->dev_id);
+	dev_set_name(&host->dev, "djtag-host-%d", host->id);
 	rc = device_register(&host->dev);
 	if (rc < 0) {
 		dev_err(&host->dev, "add_host dev register failed, rc=%d\n",
 									rc);
-		idr_remove(&djtag_hosts_idr, host->dev_id);
+		idr_remove(&djtag_hosts_idr, host->id);
 		return rc;
 	}
 
@@ -515,38 +575,43 @@ static int djtag_host_probe(struct platform_device *pdev)
 	struct resource *res;
 	int rc;
 
-	of_id = of_match_device(djtag_of_match, dev);
-	if (!of_id)
-		return -EINVAL;
-
 	host = kzalloc(sizeof(*host), GFP_KERNEL);
 	if (!host)
 		return -ENOMEM;
 
-	host->of_node = dev->of_node;
-	host->djtag_readwrite = of_id->data;
+	of_id = of_match_device(djtag_of_match, dev);
+	if (of_id) {
+		const struct hisi_djtag_ops *djtag_ops = of_id->data;
+
+		host->djtag_ops = djtag_ops;
+		host->of_node = of_node_get(dev->of_node);
+	} else {
+		rc = -EINVAL;
+		goto fail;
+	}
+
 	spin_lock_init(&host->lock);
 
 	INIT_LIST_HEAD(&host->client_list);
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (!res) {
-		dev_err(&pdev->dev, "No reg resorces!\n");
-		kfree(host);
-		return -EINVAL;
+		dev_err(dev, "No reg resorces!\n");
+		rc = -EINVAL;
+		goto fail;
 	}
 
 	if (!resource_size(res)) {
-		dev_err(&pdev->dev, "Zero reg entry!\n");
-		kfree(host);
-		return -EINVAL;
+		dev_err(dev, "Zero reg entry!\n");
+		rc = -EINVAL;
+		goto fail;
 	}
 
 	host->sysctl_reg_map = devm_ioremap_resource(dev, res);
 	if (IS_ERR(host->sysctl_reg_map)) {
 		dev_warn(dev, "Unable to map sysctl registers.\n");
-		kfree(host);
-		return -EINVAL;
+		rc = -EINVAL;
+		goto fail;
 	}
 
 	platform_set_drvdata(pdev, host);
@@ -554,11 +619,13 @@ static int djtag_host_probe(struct platform_device *pdev)
 	rc = hisi_djtag_add_host(host);
 	if (rc) {
 		dev_err(dev, "add host failed, rc=%d\n", rc);
-		kfree(host);
-		return rc;
+		goto fail;
 	}
 
 	return 0;
+fail:
+	kfree(host);
+	return rc;
 }
 
 static int djtag_host_remove(struct platform_device *pdev)
@@ -572,11 +639,14 @@ static int djtag_host_remove(struct platform_device *pdev)
 
 	list_for_each_entry_safe(client, tmp, client_list, next) {
 		list_del(&client->next);
+		device_unregister(&client->dev);
+		of_node_put(client->dev.of_node);
 		kfree(client);
 	}
 
 	device_unregister(&host->dev);
-	idr_remove(&djtag_hosts_idr, host->dev_id);
+	idr_remove(&djtag_hosts_idr, host->id);
+	of_node_put(host->of_node);
 	kfree(host);
 
 	return 0;
