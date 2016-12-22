0998d006a940b7213e590886fc3ef62792705c09
Xiaofei Tan <tanxiaofei@huawei.com>
2016-12-22 10:50:46 +0800

    {temphack} libsas: fix the bug of losing disks in hot plug test
    When one disk is unpluged, libsas will unregister it and setup
    an delay work DISCE_DESTRUCT to destroy the device at last.
    If we plug the disk before that delay work finshed, the disk
    will be regarded as an new device. At this moment, libsas find
    the device is still in domain , then disable its ex phy.
    
    We shouldn't think disk is in domain if the device state is
    SAS_DEV_DESTROY or SAS_DEV_GONE.
    
    Signed-off-by: Xiaofei Tan <tanxiaofei@huawei.com>
    Reviewed-by: Xiang Chen <chenxiang66@hisilicon.com>

--- drivers/scsi/libsas/sas_expander.c
+++ drivers/scsi/libsas/sas_expander.c
@@ -643,8 +643,14 @@ static int sas_dev_present_in_domain(struct asd_sas_port *port,
 	if (SAS_ADDR(port->sas_addr) == SAS_ADDR(sas_addr))
 		return 1;
 	list_for_each_entry(dev, &port->dev_list, dev_list_node) {
-		if (SAS_ADDR(dev->sas_addr) == SAS_ADDR(sas_addr))
+		if (SAS_ADDR(dev->sas_addr) == SAS_ADDR(sas_addr)) {
+			/* ignore the device that has gone */
+			if (test_bit(SAS_DEV_GONE, &dev->state)
+			  || test_bit(SAS_DEV_DESTROY, &dev->state))
+				continue;
+
 			return 1;
+		}
 	}
 	return 0;
 }
