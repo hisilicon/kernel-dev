b73edbee35ef125c9ede8e3508923a99dd6fa5a0
Jeremy Linton <jeremy.linton@arm.com>
2017-01-23 16:32:44 -0600

    {fromlist v14} arm64: pmu: Detect and enable multiple PMUs in an ACPI system
    Its possible that an ACPI system has multiple CPU types in it
    with differing PMU counters. Iterate the CPU's and make a determination
    about how many of each type exist in the system. Then take and create
    a PMU platform device for each type, and assign it the interrupts parsed
    from the MADT. Creating a platform device is necessary because the PMUs
    are not described as devices in the DSDT table.
    
    This code is loosely based on earlier work by Mark Salter.
    
    Signed-off-by: Jeremy Linton <jeremy.linton@arm.com>

--- drivers/perf/arm_pmu.c
+++ drivers/perf/arm_pmu.c
@@ -1068,7 +1068,13 @@ int arm_pmu_device_probe(struct platform_device *pdev,
 		if (!ret)
 			ret = init_fn(pmu);
 	} else if (probe_table) {
-		ret = probe_plat_pmu(pmu, probe_table, read_cpuid_id());
+		if (acpi_disabled) {
+			/* use the current cpu. */
+			ret = probe_plat_pmu(pmu, probe_table,
+					     read_cpuid_id());
+		} else {
+			ret = probe_plat_pmu(pmu, probe_table, pdev->id);
+		}
 	}
 
 	if (ret) {
