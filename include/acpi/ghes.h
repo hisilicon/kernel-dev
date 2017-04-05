ca0a5f32dee2c3ade1a90a26f7321221f3427f03
zhengqiang <yale.prch@gmail.com>
2017-04-05 15:39:07 +0800

    Revert "{fromlist v11}acpi: apei: handle SEA notification type for ARMv8"
    This reverts commit 5d0e65b5e9060a68b162919d8c4c759d12ef957b.

--- include/acpi/ghes.h
+++ include/acpi/ghes.h
@@ -1,6 +1,3 @@
-#ifndef GHES_H
-#define GHES_H
-
 #include <acpi/apei.h>
 #include <acpi/hed.h>
 
@@ -98,7 +95,3 @@ static inline void *acpi_hest_generic_data_payload(struct acpi_hest_generic_data
 		(void *)(((struct acpi_hest_generic_data_v300 *)(gdata)) + 1) :
 		gdata + 1;
 }
-
-void ghes_notify_sea(void);
-
-#endif /* GHES_H */
