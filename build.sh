e6ae9988cbc91b13b92d8eb0a160734769722063
Daode Huang <huangdaode@hisilicon.com>
2017-06-22 17:00:48 +0800

    {temphack} use build.sh instead of make_image.sh to compile
    Signed-off-by: Daode Huang <huangdaode@hisilicon.com>

---
+++ build.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+usage()
+{
+	echo "$0 board_type"
+	echo "Example:	"
+	echo "		$0 d01|d02|d03|d05|d06"
+	exit 1
+}
+if [ $# != 1 ];then
+	usage
+fi
+export board_type=$1
+gen_config()
+{
+	case $board_type in
+	"d01")
+		make hisi_defconfig
+	;;
+	"d02"|"d03"|"d05")
+		echo "$board_type"
+		./scripts/kconfig/merge_config.sh -m arch/arm64/configs/defconfig arch/arm64/configs/plinth-config
+	;;
+	"d06")
+	;;
+	*)
+	echo "Unsupported board_type $board_type, please try again."
+	;;
+	esac
+	mv -f .config .merged.config
+	make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- KCONFIG_ALLCONFIG=.merged.config alldefconfig
+}
+
+build_image()
+{
+	if [ "$board_type" = "d01" ];then
+		export ARCH=arm
+		export CROSS_COMPILE=arm-linux-gnueabihf-
+		make -j14 zImage
+		make hip04-d01.dtb
+		cat arch/arm/boot/zImage arch/arm/boot/dts/hip04-d01.dtb >.kernel
+	else
+		export ARCH=arm64
+		export CROSS_COMPILE=aarch64-linux-gnu-
+		make -j16
+	fi
+}
+
+build_dtb()
+{
+	echo "Building dtb ..."
+	case $board_type in
+	"d02")
+	make hisilicon/hip05-d02.dtb
+	;;
+	"d03")
+	make hisilicon/hip06-d03.dtb
+	;;
+	"d05")
+	make hisilicon/hip07-d05.dtb
+	;;
+	*)
+	echo "Unsupported dtb for board $board_type"
+	;;
+	esac
+}
+
+gen_config
+build_image
+build_dtb
