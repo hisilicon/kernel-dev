9eeb735654d2fc83e8207f9179b1f28ade575004
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2017-12-15 13:05:53 +0000

    iommu: Manage processes address spaces
    IOMMU drivers need a way to bind Linux address spaces to devices. This is
    used for Shared Virtual Addressing (SVA), where devices support paging. In
    that mode, DMA can directly target virtual addresses of a process.
    
    Introduce boilerplate code for allocating IOMMU mm structures and binding
    them to devices. Four operations are added to IOMMU drivers:
    
    * mm_alloc: to create an io_mm structure and perform architecture-specific
      operations required to grab the process (for instance on ARM, pin down
      the CPU ASID so that the process doesn't get assigned a new ASID on
      rollover).
    
      There is a single valid io_mm structure per Linux mm. Future extensions
      may also use io_mm for kernel-managed address spaces, populated with
      map()/unmap() calls instead of bound to process address spaces. This
      patch focuses on "shared" io_mm.
    
    * mm_attach: attach an mm to a device. The IOMMU driver checks that the
      device is capable of sharing an address space, and writes the PASID
      table entry to install the process page directory.
    
      Some IOMMU drivers will have a single PASID table per domain, for
      convenience. Other can implement it differently but to help these
      drivers, mm_attach and mm_detach take a 'first' or 'last' parameter
      telling whether they need to install/remove the PASID entry or only send
      the required TLB invalidations.
    
    * mm_detach: detach an mm from a device. The IOMMU driver removes the
      PASID table entry and invalidates the IOTLBs.
    
    * mm_free: free a structure allocated by mm_alloc, and let arch release
      the process.
    
    mm_attach and mm_detach operations are serialized with a spinlock. At the
    moment it is global, but if we try to optimize it, the core should at
    least prevent concurrent attach/detach on the same domain (so multi-level
    PASID table code can allocate tables lazily). mm_alloc can sleep, but
    mm_free must not (because we'll have to call it from call_srcu later on.)
    
    At the moment we use an IDR for allocating PASIDs and retrieving contexts.
    We also use a single spinlock. These can be refined and optimized later (a
    custom allocator will be needed for top-down PASID allocation).
    
    Keeping track of address spaces requires the use of MMU notifiers.
    Handling process exit with regard to unbind() is tricky, so it is left for
    another patch and we explicitly fail mm_alloc for the moment.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>

--- drivers/iommu/iommu-sva.c
+++ drivers/iommu/iommu-sva.c
@@ -7,10 +7,205 @@
  * SPDX-License-Identifier: GPL-2.0
  */
 
+#include <linux/idr.h>
 #include <linux/iommu.h>
+#include <linux/slab.h>
+#include <linux/spinlock.h>
 
 /* TODO: stub for the fault queue. Remove later. */
 #define iommu_fault_queue_flush(...)
+
+/* Link between a domain and a mm */
+struct iommu_context {
+	struct io_mm		*io_mm;
+	struct iommu_domain	*domain;
+
+	struct list_head	mm_head;
+	struct list_head	domain_head;
+
+	/* Number of devices that use this context */
+	refcount_t		ref;
+};
+
+/*
+ * Because we're using an IDR, PASIDs are limited to 31 bits (the sign bit is
+ * used for returning errors). In practice implementations will use at most 20
+ * bits, which is the PCI limit.
+ */
+static DEFINE_IDR(iommu_pasid_idr);
+
+/*
+ * For the moment this is an all-purpose lock. It serializes
+ * access/modifications to contexts (mm-domain links), access/modifications
+ * to the PASID IDR, and changes to io_mm refcount as well.
+ */
+static DEFINE_SPINLOCK(iommu_sva_lock);
+
+static struct io_mm *
+io_mm_alloc(struct iommu_domain *domain, struct mm_struct *mm)
+{
+	int err;
+	int pasid;
+	struct io_mm *io_mm;
+
+	if (WARN_ON(!domain->ops->mm_alloc || !domain->ops->mm_free))
+		return ERR_PTR(-ENODEV);
+
+	io_mm = domain->ops->mm_alloc(domain, mm);
+	if (IS_ERR(io_mm))
+		return io_mm;
+	if (!io_mm)
+		return ERR_PTR(-ENOMEM);
+
+	io_mm->mm		= mm;
+	io_mm->release		= domain->ops->mm_free;
+	INIT_LIST_HEAD(&io_mm->domains);
+
+	idr_preload(GFP_KERNEL);
+	spin_lock(&iommu_sva_lock);
+	pasid = idr_alloc_cyclic(&iommu_pasid_idr, io_mm, domain->min_pasid,
+				 domain->max_pasid + 1, GFP_ATOMIC);
+	io_mm->pasid = pasid;
+	spin_unlock(&iommu_sva_lock);
+	idr_preload_end();
+
+	if (pasid < 0) {
+		err = pasid;
+		goto err_free_mm;
+	}
+
+	/* TODO: keep track of mm. For the moment, abort. */
+	err = -ENOSYS;
+	spin_lock(&iommu_sva_lock);
+	idr_remove(&iommu_pasid_idr, io_mm->pasid);
+	spin_unlock(&iommu_sva_lock);
+
+err_free_mm:
+	domain->ops->mm_free(io_mm);
+
+	return ERR_PTR(err);
+}
+
+static void io_mm_free(struct io_mm *io_mm)
+{
+	void (*release)(struct io_mm *);
+
+	release = io_mm->release;
+
+	release(io_mm);
+}
+
+static void io_mm_release(struct kref *kref)
+{
+	struct io_mm *io_mm;
+
+	io_mm = container_of(kref, struct io_mm, kref);
+	WARN_ON(!list_empty(&io_mm->domains));
+
+	idr_remove(&iommu_pasid_idr, io_mm->pasid);
+
+	io_mm_free(io_mm);
+}
+
+/*
+ * Returns non-zero if a reference to the io_mm was successfully taken.
+ * Returns zero if the io_mm is being freed and should not be used.
+ */
+static int io_mm_get_locked(struct io_mm *io_mm)
+{
+	if (io_mm)
+		return kref_get_unless_zero(&io_mm->kref);
+
+	return 0;
+}
+
+static void io_mm_put_locked(struct io_mm *io_mm)
+{
+	kref_put(&io_mm->kref, io_mm_release);
+}
+
+static void io_mm_put(struct io_mm *io_mm)
+{
+	spin_lock(&iommu_sva_lock);
+	io_mm_put_locked(io_mm);
+	spin_unlock(&iommu_sva_lock);
+}
+
+static int io_mm_attach(struct iommu_domain *domain, struct device *dev,
+			struct io_mm *io_mm)
+{
+	int err;
+	int pasid = io_mm->pasid;
+	struct iommu_context *context;
+
+	if (WARN_ON(!domain->ops->mm_attach || !domain->ops->mm_detach))
+		return -ENODEV;
+
+	if (pasid > domain->max_pasid || pasid < domain->min_pasid)
+		return -ENOSPC;
+
+	context = kzalloc(sizeof(*context), GFP_KERNEL);
+	if (!context)
+		return -ENOMEM;
+
+	context->io_mm		= io_mm;
+	context->domain		= domain;
+	refcount_set(&context->ref, 1);
+
+	spin_lock(&iommu_sva_lock);
+	err = domain->ops->mm_attach(domain, dev, io_mm, true);
+	if (err) {
+		kfree(context);
+		spin_unlock(&iommu_sva_lock);
+		return err;
+	}
+
+	list_add(&context->mm_head, &io_mm->domains);
+	list_add(&context->domain_head, &domain->mm_list);
+	spin_unlock(&iommu_sva_lock);
+
+	return 0;
+}
+
+static void iommu_context_free(struct iommu_context *context)
+{
+	if (WARN_ON(!context->io_mm || !context->domain))
+		return;
+
+	list_del(&context->mm_head);
+	list_del(&context->domain_head);
+	io_mm_put_locked(context->io_mm);
+
+	kfree(context);
+}
+
+/* Attach an existing context to the device */
+static int io_mm_attach_locked(struct iommu_context *context,
+			       struct device *dev)
+{
+	int err = context->domain->ops->mm_attach(context->domain, dev,
+						  context->io_mm, false);
+
+	if (!err)
+		refcount_inc(&context->ref);
+	return err;
+}
+
+/* Detach device from context and release it if necessary */
+static void io_mm_detach_locked(struct iommu_context *context,
+				struct device *dev)
+{
+	bool last = false;
+	struct iommu_domain *domain = context->domain;
+
+	if (refcount_dec_and_test(&context->ref))
+		last = true;
+
+	domain->ops->mm_detach(domain, dev, context->io_mm, last);
+
+	if (last)
+		iommu_context_free(context);
+}
 
 /**
  * iommu_sva_bind_device - Bind a process address space to a device
@@ -29,12 +224,61 @@
 int iommu_sva_bind_device(struct device *dev, struct mm_struct *mm, int *pasid,
 			  int flags)
 {
+	int i, err;
+	struct io_mm *io_mm = NULL;
 	struct iommu_domain *domain;
+	struct iommu_context *cur_context;
+	struct iommu_context *context = NULL;
+
 	domain = iommu_get_domain_for_dev(dev);
 	if (WARN_ON(!domain))
 		return -EINVAL;
 
-	return -ENOSYS; /* TODO */
+	/* If an io_mm already exists, use it */
+	spin_lock(&iommu_sva_lock);
+	idr_for_each_entry(&iommu_pasid_idr, io_mm, i) {
+		if (io_mm->mm != mm)
+			continue;
+
+		if (!io_mm_get_locked(io_mm)) {
+			/* Process is defunct, create a new one */
+			io_mm = NULL;
+			break;
+		}
+
+		*pasid = io_mm->pasid;
+
+		/* Great, is it also bound to this domain? */
+		list_for_each_entry(cur_context, &io_mm->domains, mm_head) {
+			if (cur_context->domain != domain)
+				continue;
+
+			io_mm_put_locked(io_mm);
+			context = cur_context;
+
+			/* Splendid, tell the driver and increase the ref */
+			err = io_mm_attach_locked(context, dev);
+			break;
+		}
+		break;
+	}
+	spin_unlock(&iommu_sva_lock);
+
+	if (context)
+		return err;
+
+	if (!io_mm) {
+		io_mm = io_mm_alloc(domain, mm);
+		if (IS_ERR(io_mm))
+			return PTR_ERR(io_mm);
+		*pasid = io_mm->pasid;
+	}
+
+	err = io_mm_attach(domain, dev, io_mm);
+	if (err)
+		io_mm_put(io_mm);
+
+	return err;
 }
 EXPORT_SYMBOL_GPL(iommu_sva_bind_device);
 
@@ -48,7 +292,10 @@ EXPORT_SYMBOL_GPL(iommu_sva_bind_device);
  */
 int iommu_sva_unbind_device(struct device *dev, int pasid)
 {
+	struct io_mm *io_mm;
 	struct iommu_domain *domain;
+	struct iommu_context *cur_context;
+	struct iommu_context *context = NULL;
 
 	domain = iommu_get_domain_for_dev(dev);
 	if (WARN_ON(!domain))
@@ -60,7 +307,25 @@ int iommu_sva_unbind_device(struct device *dev, int pasid)
 	 */
 	iommu_fault_queue_flush(dev);
 
-	return -ENOSYS; /* TODO */
+	spin_lock(&iommu_sva_lock);
+	io_mm = idr_find(&iommu_pasid_idr, pasid);
+	if (!io_mm) {
+		spin_unlock(&iommu_sva_lock);
+		return -ESRCH;
+	}
+
+	list_for_each_entry(cur_context, &io_mm->domains, mm_head) {
+		if (cur_context->domain == domain) {
+			context = cur_context;
+			break;
+		}
+	}
+
+	if (context)
+		io_mm_detach_locked(context, dev);
+	spin_unlock(&iommu_sva_lock);
+
+	return context ? 0 : -ESRCH;
 }
 EXPORT_SYMBOL_GPL(iommu_sva_unbind_device);
 
@@ -72,6 +337,14 @@ EXPORT_SYMBOL_GPL(iommu_sva_unbind_device);
  */
 void __iommu_sva_unbind_dev_all(struct iommu_domain *domain, struct device *dev)
 {
+	struct iommu_context *context, *next;
+
 	iommu_fault_queue_flush(dev);
+
+	spin_lock(&iommu_sva_lock);
+	list_for_each_entry_safe(context, next, &domain->mm_list, domain_head)
+		io_mm_detach_locked(context, dev);
+	spin_unlock(&iommu_sva_lock);
+
 }
 EXPORT_SYMBOL_GPL(__iommu_sva_unbind_dev_all);
--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -94,6 +94,19 @@ struct iommu_domain {
 	void *handler_token;
 	struct iommu_domain_geometry geometry;
 	void *iova_cookie;
+
+	unsigned int min_pasid, max_pasid;
+	struct list_head mm_list;
+};
+
+struct io_mm {
+	int			pasid;
+	struct list_head	domains;
+	struct kref		kref;
+	struct mm_struct	*mm;
+
+	/* Release callback for this mm */
+	void (*release)(struct io_mm *io_mm);
 };
 
 enum iommu_cap {
@@ -164,6 +177,11 @@ struct iommu_resv_region {
  * @domain_free: free iommu domain
  * @attach_dev: attach device to an iommu domain
  * @detach_dev: detach device from an iommu domain
+ * @mm_alloc: allocate io_mm
+ * @mm_free: free io_mm
+ * @mm_attach: attach io_mm to a domain
+ * @mm_detach: detach io_mm from a domain. Remove PASID entry and
+ *             flush associated TLB entries.
  * @map: map a physically contiguous memory region to an iommu domain
  * @unmap: unmap a physically contiguous memory region from an iommu domain
  * @map_sg: map a scatter-gather list of physically contiguous memory chunks
@@ -197,6 +215,13 @@ struct iommu_ops {
 
 	int (*attach_dev)(struct iommu_domain *domain, struct device *dev);
 	void (*detach_dev)(struct iommu_domain *domain, struct device *dev);
+	struct io_mm *(*mm_alloc)(struct iommu_domain *domain,
+				  struct mm_struct *mm);
+	void (*mm_free)(struct io_mm *io_mm);
+	int (*mm_attach)(struct iommu_domain *domain, struct device *dev,
+			 struct io_mm *io_mm, bool first);
+	void (*mm_detach)(struct iommu_domain *domain, struct device *dev,
+			  struct io_mm *io_mm, bool last);
 	int (*map)(struct iommu_domain *domain, unsigned long iova,
 		   phys_addr_t paddr, size_t size, int prot);
 	size_t (*unmap)(struct iommu_domain *domain, unsigned long iova,
