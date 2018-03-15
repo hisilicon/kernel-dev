36e8261f6f17019085295e549126cfb7cd30903b
Xiaofei Tan <tanxiaofei@huawei.com>
2018-03-15 08:55:15 +0800

    {topost} scsi: hisi_sas: block IO when host controller is during reset
    When host controller is resetting, hisi_sas_task_exec() just returns
    -EINVAL directly in current driver. This may cause SMP IO failure, and
    the failure SMP IO won't be going to retry. This is unacceptable for
    libsas.
    
    This patch uses hisi_hba->sem to block IO during host reset.
    
    Signed-off-by: Xiaofei Tan <tanxiaofei@huawei.com>
    Signed-off-by: John Garry <john.garry@huawei.com>

--- drivers/scsi/hisi_sas/hisi_sas_main.c
+++ drivers/scsi/hisi_sas/hisi_sas_main.c
@@ -513,8 +513,12 @@ static int hisi_sas_task_exec(struct sas_task *task, gfp_t gfp_flags,
 	struct device *dev = hisi_hba->dev;
 	struct hisi_sas_dq *dq = NULL;
 
-	if (unlikely(test_bit(HISI_SAS_REJECT_CMD_BIT, &hisi_hba->flags)))
-		return -EINVAL;
+	if (unlikely(test_bit(HISI_SAS_REJECT_CMD_BIT, &hisi_hba->flags))) {
+		dev_info(dev, "task exec: IO blocking\n");
+		down(&hisi_hba->sem);
+		up(&hisi_hba->sem);
+		dev_info(dev, "task exec: IO blocking ended\n");
+	}
 
 	/* protect task_prep and start_delivery sequence */
 	rc = hisi_sas_task_prep(task, &dq, is_tmf, tmf, &pass);
