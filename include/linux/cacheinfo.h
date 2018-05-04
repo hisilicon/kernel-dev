2800083775b1ff9992721ffad9ed55348bb14adc
Jeremy Linton <jeremy.linton@arm.com>
2018-05-04 19:33:52 +0800

    {fromlist v8}drivers: base cacheinfo: Add support for ACPI based firmware tables
    Call ACPI cache parsing routines from base cacheinfo code if ACPI
    is enable. Also stub out cache_setup_acpi() so that individual
    architectures can enable ACPI topology parsing.
    
    Signed-off-by: Jeremy Linton <jeremy.linton@arm.com>
    Signed-off-by: Xie XiuQi <xiexiuqi@huawei.com>

--- include/linux/cacheinfo.h
+++ include/linux/cacheinfo.h
@@ -97,6 +97,16 @@ int func(unsigned int cpu)					\
 struct cpu_cacheinfo *get_cpu_cacheinfo(unsigned int cpu);
 int init_cache_level(unsigned int cpu);
 int populate_cache_leaves(unsigned int cpu);
+int cache_setup_acpi(unsigned int cpu);
+#ifndef CONFIG_ACPI
+static inline int acpi_find_last_cache_level(unsigned int cpu)
+{
+	/* ACPI kernels should be built with PPTT support */
+	return 0;
+}
+#else
+int acpi_find_last_cache_level(unsigned int cpu);
+#endif
 
 const struct attribute_group *cache_get_priv_group(struct cacheinfo *this_leaf);
 
