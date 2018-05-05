3360e0217fcdeb3efcb77c37e70c3215fc341911
Jason Yan <yanaijie@huawei.com>
2018-05-05 11:17:52 +0800

    {topost} scsi: libsas: check if the same sata device when flutter
    The ata device do not have a real sas address. If a ata device is
    replaced with another one, the sas address is the same. Now libsas treat
    this senario as flutter and do not delete the old one and discover the
    new one. This will cause the date read from or write to the wrong
    device.
    
    And also when hotplugging a sata device, libsas entered to the flutter
    case and found the phy attached address is abnormal.
    
    Fix this issue by checking the phy attached address and the ata device's
    class and id.
    
    Signed-off-by: Jason Yan <yanaijie@huawei.com>

--- drivers/scsi/libsas/sas_expander.c
+++ drivers/scsi/libsas/sas_expander.c
@@ -2008,6 +2008,37 @@ static bool dev_type_flutter(enum sas_device_type new, enum sas_device_type old)
 	return false;
 }
 
+static bool sas_check_after_flutter(struct ex_phy *phy, struct domain_device *ata_dev,
+			      u8 *sas_addr)
+{
+	/* the phy attached address will be updated by sas_ex_phy_discover()
+	 * and sometimes become abnormal
+	 */
+	if (SAS_ADDR(phy->attached_sas_addr) != SAS_ADDR(sas_addr) ||
+	    SAS_ADDR(phy->attached_sas_addr) == 0) {
+		/* if attached_sas_addr become abnormal, we must set the
+		 * original address back so that the device can be unregistered
+		 */
+		memcpy(phy->attached_sas_addr, sas_addr, SAS_ADDR_SIZE);
+		SAS_DPRINTK("phy address(%016llx) abnormal, origin:%016llx\n",
+			    SAS_ADDR(phy->attached_sas_addr),
+			    SAS_ADDR(sas_addr));
+		return false;
+	}
+
+	if (ata_dev) {
+		struct ata_device *dev = sas_to_ata_dev(ata_dev);
+		unsigned int class = ata_dev->sata_dev.class;
+		u16 *id = ata_dev->sata_dev.id;
+
+		/* to see if the disk is replaced with another one */
+		if (!ata_dev_same_device(dev, class, id))
+			return false;
+	}
+
+	return true;
+}
+
 static int sas_rediscover_dev(struct domain_device *dev, int phy_id, bool last)
 {
 	struct expander_device *ex = &dev->ex_dev;
@@ -2052,7 +2083,12 @@ static int sas_rediscover_dev(struct domain_device *dev, int phy_id, bool last)
 			action = ", needs recovery";
 		SAS_DPRINTK("ex %016llx phy 0x%x broadcast flutter%s\n",
 			    SAS_ADDR(dev->sas_addr), phy_id, action);
-		return res;
+
+		/* if we failed something, continue to unregister,
+		 * otherwise keep the device status
+		 */
+		if (sas_check_after_flutter(phy, ata_dev, sas_addr))
+			return res;
 	}
 
 	/* we always have to delete the old device when we went here */
