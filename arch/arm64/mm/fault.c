484ceaceeccd37ee6672589e901cb898acf8769f
James Morse <james.morse@arm.com>
2018-05-16 17:28:29 +0100

    {fromlist v4} arm64: acpi: Make apei_claim_sea() synchronise with APEI's irq work
    APEI is unable to do all of its error handling work in nmi-context, so
    it defers non-fatal work onto the irq_work queue. arch_irq_work_raise()
    sends an IPI to the calling cpu, but we can't guarantee this will be
    taken before we return.
    
    Unless we interrupted a context with irqs-masked, we can call
    irq_work_run() to do the work now. Otherwise return -EINPROGRESS to
    indicate ghes_notify_sea() found some work to do, but it hasn't
    finished yet.
    
    With this we can take apei_claim_sea() returning '0' to mean this
    external-abort was also notification of a firmware-first RAS error,
    and that APEI has processed the CPER records.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Reviewed-by: Punit Agrawal <punit.agrawal@arm.com>
    Tested-by: Tyler Baicar <tbaicar@codeaurora.org>
    CC: Xie XiuQi <xiexiuqi@huawei.com>
    CC: gengdongjiu <gengdongjiu@huawei.com>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -568,11 +568,10 @@ static int do_sea(unsigned long addr, unsigned int esr, struct pt_regs *regs)
 
 	inf = esr_to_fault_info(esr);
 
-	/*
-	 * Return value ignored as we rely on signal merging.
-	 * Future patches will make this more robust.
-	 */
-	apei_claim_sea(regs);
+	if (apei_claim_sea(regs) == 0) {
+		/* APEI claimed this as a firmware-first notification */
+		return 0;
+	}
 
 	if (user_mode(regs)) {
 		if (test_thread_flag(TIF_SEA_NOTIFY))
