93b7644cfaefa2d10a8a89d1b962945642ea7125
Lijun Ou <oulijun@huawei.com>
2018-07-23 19:50:46 +0800

    {temphack} RDMA/hns: Disable rq inline function
    This patch mainly disable rq inline function in default configuration.
    
    Signed-off-by: Lijun Ou <oulijun@huawei.com>

--- drivers/infiniband/hw/hns/hns_roce_hw_v2.c
+++ drivers/infiniband/hw/hns/hns_roce_hw_v2.c
@@ -1297,7 +1297,6 @@ static int hns_roce_v2_profile(struct hns_roce_dev *hr_dev)
 
 	caps->flags		= HNS_ROCE_CAP_FLAG_REREG_MR |
 				  HNS_ROCE_CAP_FLAG_ROCE_V1_V2 |
-				  HNS_ROCE_CAP_FLAG_RQ_INLINE |
 				  HNS_ROCE_CAP_FLAG_RECORD_DB |
 				  HNS_ROCE_CAP_FLAG_SQ_RECORD_DB;
 	caps->pkey_table_len[0] = 1;
