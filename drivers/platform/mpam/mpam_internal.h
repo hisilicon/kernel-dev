96d7c0b933c0334abf9c45d73f239ac3f382c074
James Morse <james.morse@arm.com>
2019-02-08 17:31:13 +0000

    arm_mpam: Summarize feature support during mpam_enable()
    To make a decision about whether to expose an mpam class as
    a resctrl resource we need to know its overall supported
    features and properties.
    
    Once we've probed all the devices, we can walk the tree
    and produced overall values. If bitmap properties are mismatched
    within a component we cannot support that bitmap.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- drivers/platform/mpam/mpam_internal.h
+++ drivers/platform/mpam/mpam_internal.h
@@ -113,6 +113,7 @@ struct mpam_component
 	struct rdt_domain       resctrl_domain;
 	struct cpumask          fw_affinity;
 
+
 	/* member of mpam_class:components */
 	struct list_head        class_list;
 };
@@ -138,6 +139,16 @@ struct mpam_class
 
 	/* level is stored in resctrl_res.cache_level, regardless of type */
 	enum mpam_class_types	type;
+
+	/* Once enabled, the common features */
+	mpam_features_t		features;
+
+	/* The common values if the feature is supported */
+	u16			cpbm_wd;
+	u16			mbw_pbm_bits;
+	u16			bwa_wd;
+	u16			num_csu_mon;
+	u16			num_mbwu_mon;
 
 	/* member of mpam_classes */
 	struct list_head        classes_list_rcu;
