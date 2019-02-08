6e57af4cab1d58d5060c28596973a0126962dd19
James Morse <james.morse@arm.com>
2019-02-08 14:46:42 +0000

    arm_mpam: probe supported partid/pmg ranges from devices
    Once we know where all the devices are, we can register cpu hotplug
    callbacks to probe the devices each CPU can access. Once we've probed
    all the devices, we can enable MPAM.
    
    As a first step, we learn whether the MSC supports MPAMv1.x, and
    update our system wide view of the commonly supported partid/pmg range.
    
    As noted in the ACPI code, we learn the cache affinities as CPUs
    come online. This ensures the data we export via resctrl matches
    the data cacheinfo exports via sysfs.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- drivers/platform/mpam/mpam_devices.c
+++ drivers/platform/mpam/mpam_devices.c
@@ -12,10 +12,12 @@
 #include <linux/list.h>
 #include <linux/lockdep.h>
 #include <linux/mutex.h>
+#include <linux/preempt.h>
 #include <linux/printk.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/types.h>
+#include <linux/workqueue.h>
 
 #include <asm/mpam.h>
 
@@ -32,6 +34,33 @@ static LIST_HEAD(mpam_all_devices);
 
 /* Classes are the set of MSCs that make up components of the same type. */
 LIST_HEAD(mpam_classes);
+
+/* Hold this when registering/unregistering cpuhp callbacks */
+static DEFINE_MUTEX(mpam_cpuhp_lock);
+static int mpam_cpuhp_state;
+
+struct mpam_sysprops mpam_sysprops;
+
+/*
+ * mpam is enabled once all devices have been probed from CPU online callbacks,
+ * scheduled via this work_struct.
+ */
+static struct work_struct mpam_enable_work;
+
+static inline u32 mpam_read_reg(struct mpam_device *dev, u16 reg)
+{
+	WARN_ON_ONCE(reg > SZ_MPAM_DEVICE);
+	assert_spin_locked(&dev->lock);
+
+	/*
+	 * If we touch a device that isn't accessible from this CPU we may get
+	 * an external-abort.
+	 */
+	WARN_ON_ONCE(preemptible());
+	WARN_ON_ONCE(!cpumask_test_cpu(smp_processor_id(), &dev->fw_affinity));
+
+	return readl_relaxed(dev->mapped_hwpage + reg);
+}
 
 static struct mpam_device * __init
 mpam_device_alloc(struct mpam_component *comp)
@@ -239,17 +268,191 @@ void __init mpam_device_set_overflow_irq(struct mpam_device *dev, u32 irq,
 	spin_unlock(&dev->lock);
 }
 
+static void mpam_probe_update_sysprops(u16 max_partid, u8 max_pmg)
+{
+	lockdep_assert_held(&mpam_devices_lock);
+
+	mpam_sysprops.max_partid = min(mpam_sysprops.max_partid, max_partid);
+	mpam_sysprops.max_pmg = min(mpam_sysprops.max_pmg, max_pmg);
+}
+
+static int mpam_device_probe(struct mpam_device *dev)
+{
+	u32 hwfeatures;
+	u16 max_partid, max_pmg;
+
+	if (mpam_read_reg(dev, MPAMF_AIDR) != MPAM_ARCHITECTURE_V1) {
+		pr_err_once("device at 0x%llx does not match MPAM architecture v1.0\n",
+			    dev->hwpage_address);
+		return -EIO;
+	}
+
+	hwfeatures = mpam_read_reg(dev, MPAMF_IDR);
+	max_partid = hwfeatures & MPAMF_IDR_PARTID_MAX_MASK;
+	max_pmg = (hwfeatures & MPAMF_IDR_PMG_MAX_MASK) >> MPAMF_IDR_PMG_MAX_SHIFT;
+
+	mpam_probe_update_sysprops(max_partid, max_pmg);
+
+	dev->probed = true;
+
+	return 0;
+}
+
+/*
+ * Enable mpam once all devices have been probed.
+ * Scheduled by mpam_discovery_complete() once all devices have been created.
+ * Also scheduled when new devices are probed when new CPUs come online.
+ */
+static void mpam_enable(struct work_struct *work)
+{
+	struct mpam_device *dev;
+	bool all_devices_probed = true;
+
+	/* Have we probed all the devices? */
+	mutex_lock(&mpam_devices_lock);
+	list_for_each_entry(dev, &mpam_all_devices, glbl_list) {
+		spin_lock(&dev->lock);
+		if (!dev->probed)
+			all_devices_probed = false;
+		spin_unlock(&dev->lock);
+
+		if (!all_devices_probed)
+			break;
+	}
+	mutex_unlock(&mpam_devices_lock);
+
+	if (!all_devices_probed)
+		return;
+
+}
+
+
 int __init mpam_discovery_start(void)
 {
 	if (!mpam_cpus_have_feature())
 		return -EOPNOTSUPP;
+
+	mpam_sysprops.max_partid = mpam_cpu_max_partids();
+	mpam_sysprops.max_pmg = mpam_cpu_max_pmgs();
+
+	INIT_WORK(&mpam_enable_work, mpam_enable);
+
+	return 0;
+}
+
+/*
+ * Firmware didn't give us an affinity, but a cache-id, if this cpu has that
+ * cache-id, update the fw_affinity for this component.
+ */
+static void mpam_sync_cpu_cache_component_fw_affinity(struct mpam_class *class,
+						      int cpu)
+{
+	int cpu_cache_id;
+	struct mpam_component *comp;
+
+	lockdep_assert_held(&mpam_devices_lock);
+
+	if (class->type != MPAM_CLASS_CACHE)
+		return;
+
+	cpu_cache_id = get_cpu_cacheinfo_id(cpu, class->level);
+	comp = mpam_component_get(class, cpu_cache_id, false);
+
+	/* This cpu does not have a component of this class */
+	if (!comp)
+		return;
+
+	cpumask_set_cpu(cpu, &comp->fw_affinity);
+	cpumask_set_cpu(cpu, &class->fw_affinity);
+}
+
+static int __online_devices(struct mpam_component *comp, int cpu)
+{
+	int err = 0;
+	struct mpam_device *dev;
+	bool new_device_probed = false;
+
+	list_for_each_entry(dev, &comp->devices, comp_list) {
+		if (!cpumask_test_cpu(cpu, &dev->fw_affinity))
+			continue;
+
+
+		spin_lock(&dev->lock);
+		if (!dev->probed) {
+			err = mpam_device_probe(dev);
+			if (!err)
+				new_device_probed = true;
+		}
+
+		cpumask_set_cpu(cpu, &dev->online_affinity);
+		spin_unlock(&dev->lock);
+
+		if (err)
+			return err;
+	}
+
+	if (new_device_probed)
+		return 1;
+
+	return 0;
+}
+
+static int mpam_cpu_online(unsigned int cpu)
+{
+	int err = 0;
+	struct mpam_class *class;
+	struct mpam_component *comp;
+	bool new_device_probed = false;
+
+	mutex_lock(&mpam_devices_lock);
+	list_for_each_entry(class, &mpam_classes, classes_list) {
+		mpam_sync_cpu_cache_component_fw_affinity(class, cpu);
+
+		list_for_each_entry(comp, &class->components, class_list) {
+			if (!cpumask_test_cpu(cpu, &comp->fw_affinity))
+				continue;
+
+			err = __online_devices(comp, cpu);
+			if (err > 0)
+				new_device_probed = true;
+			if (err < 0)
+				break; // mpam_broken
+		}
+	}
+
+	if (new_device_probed && err >= 0)
+		schedule_work(&mpam_enable_work);
+
+	mutex_unlock(&mpam_devices_lock);
+
+	if (err < 0)
+		return err;
+
+	return 0;
+}
+
+static int mpam_cpu_offline(unsigned int cpu)
+{
+	struct mpam_device *dev;
+
+	mutex_lock(&mpam_devices_lock);
+	list_for_each_entry(dev, &mpam_all_devices, glbl_list)
+		cpumask_clear_cpu(cpu, &dev->online_affinity);
+
+	mutex_unlock(&mpam_devices_lock);
 
 	return 0;
 }
 
 void __init mpam_discovery_complete(void)
 {
-
+	mutex_lock(&mpam_cpuhp_lock);
+	mpam_cpuhp_state = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN,
+					     "mpam:online", mpam_cpu_online,
+					     mpam_cpu_offline);
+	if (mpam_cpuhp_state <= 0)
+		pr_err("Failed to register 'dyn' cpuhp callbacks");
+	mutex_unlock(&mpam_cpuhp_lock);
 }
 
 void mpam_discovery_failed(void)
