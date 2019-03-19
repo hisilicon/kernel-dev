9e54292b2f537d3bcc629b7534e63cbe663f5c65
James Morse <james.morse@arm.com>
2019-03-19 12:16:38 +0000

    x86/resctrl: Switch closid setting/matchingto use helpers
    When CDP is enabled Intel RDT uses closids in pairs, odd for code
    even for data. We previously split the resctrl code up so that the
    filesystem parts only worried about one closid, which with CDP had
    two configurations. When calling the arch code to update the
    configuration, we mapped the resctrl:closid to a hw_closid_t.
    
    Add helpers for setting the closid for a task. Abstracting this will
    allow another architecture to store different values in task_struct.
    
    For MPAM, arm64 needs to store two closids and two rmids in task_struct,
    one for each of code/data as there is no odd/even pairing.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/x86/include/asm/resctrl.h
+++ arch/x86/include/asm/resctrl.h
@@ -4,6 +4,13 @@
 #ifndef __ASM_RESCTRL_H__
 #define __ASM_RESCTRL_H__
 
+#include <linux/bug.h>
+#include <linux/sched.h>
+
+typedef struct { u32 val; } hw_closid_t;
+#define as_hwclosid_t(x)	((hw_closid_t){(x)})
+#define hwclosid_val(x)		(x.val)
+
 /*
  * Get the system wide closid/rmid limits. The number of closids should not be
  * affected by CDP.
@@ -11,4 +18,36 @@
 u32 resctrl_arch_system_num_closid(void);
 u32 resctrl_arch_system_num_rmid(void);
 
-#endif /* __ASM_RESCTRL_H__ */
+static inline void resctrl_arch_set_closid(struct task_struct *tsk,
+					   hw_closid_t closid_code,
+					   hw_closid_t closid_data)
+{
+	u32 c = hwclosid_val(closid_code);
+	u32 d = hwclosid_val(closid_data);
+
+	/* For CDP the code/data closid must be adjacent even/odd pairs */
+	if (c != d) {
+		WARN_ON_ONCE(d + 1 != c);
+		tsk->closid = d >> 1;
+	} else {
+		tsk->closid = d;
+	}
+}
+
+static inline bool resctrl_arch_match_closid(struct task_struct *tsk,
+					     hw_closid_t closid_code,
+					     hw_closid_t closid_data)
+{
+	u32 c = hwclosid_val(closid_code);
+	u32 d = hwclosid_val(closid_data);
+
+	/* For CDP the code/data closid must be adjacent even/odd pairs */
+	if (c != d) {
+		WARN_ON_ONCE(d + 1 != c);
+		return tsk->closid == d >> 1;
+	} else {
+		return tsk->closid == d;
+	}
+}
+
+#endif /* _ASM_RESCTRL_H_ */
