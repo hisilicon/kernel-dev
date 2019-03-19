e41c232c025f15ebd7afcceb03865c7da5b7ed43
James Morse <james.morse@arm.com>
2019-03-19 16:45:54 +0000

    x86/resctrl: Export resctrl fs's init function
    rdtgroup_init() needs exporting so that arch code can call it once
    it lives in core code. As this is one of the few functions we export,
    rename it to have the filesystems name.
    
    x86's arch code init functions for RDT need renaming (again) to
    avoid duplicate definitions.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -884,7 +884,7 @@ void resctrl_cpu_detect(struct cpuinfo_x86 *c)
 	}
 }
 
-static int __init resctrl_late_init(void)
+static int __init resctrl_arch_late_init(void)
 {
 	struct rdt_resource *r;
 	int state, ret;
@@ -909,7 +909,7 @@ static int __init resctrl_late_init(void)
 	if (state < 0)
 		return state;
 
-	ret = rdtgroup_init();
+	ret = resctrl_init();
 	if (ret) {
 		cpuhp_remove_state(state);
 		return ret;
@@ -925,12 +925,12 @@ static int __init resctrl_late_init(void)
 	return 0;
 }
 
-late_initcall(resctrl_late_init);
+late_initcall(resctrl_arch_late_init);
 
-static void __exit resctrl_exit(void)
+static void __exit resctrl_arch_exit(void)
 {
 	cpuhp_remove_state(rdt_online);
-	rdtgroup_exit();
+	resctrl_exit();
 }
 
-__exitcall(resctrl_exit);
+__exitcall(resctrl_arch_exit);
--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -284,4 +284,7 @@ void resctrl_arch_reset_rmid(struct rdt_resource *r, struct rdt_domain *d,
 extern unsigned int resctrl_rmid_realloc_threshold;
 extern unsigned int resctrl_rmid_realloc_limit;
 
+int __init resctrl_init(void);
+void __exit resctrl_exit(void);
+
 #endif /* _RESCTRL_H */
