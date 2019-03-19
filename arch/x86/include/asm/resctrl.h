46d3643fb2721a5bf88724f5361647481e762c77
James Morse <james.morse@arm.com>
2019-03-19 16:12:58 +0000

    x86/resctrl: Move alloc/mon static keys into helpers
    resctrl enables three static keys depending on the features it has enabled.
    Another architecture's context switch code may look different, any
    static keys that control it should be buried behind helpers.
    
    Move the alloc/mon logic into arch-specific helpers as a preparatory step
    for making the rdt_enable_key's status something the arch code decides.
    
    This means other architectures don't have to mirror the static keys.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/x86/include/asm/resctrl.h
+++ arch/x86/include/asm/resctrl.h
@@ -5,6 +5,7 @@
 #define __ASM_RESCTRL_H__
 
 #include <linux/bug.h>
+#include <linux/jump_label.h>
 #include <linux/sched.h>
 
 #include <asm/processor.h>
@@ -17,12 +18,36 @@ typedef struct { u32 val; } hw_closid_t;
 #define RMID_VAL_UNAVAIL		BIT_ULL(62)
 #define MBM_CNTR_WIDTH			24
 
+DECLARE_STATIC_KEY_FALSE(rdt_enable_key);
+DECLARE_STATIC_KEY_FALSE(rdt_alloc_enable_key);
+DECLARE_STATIC_KEY_FALSE(rdt_mon_enable_key);
+
 /*
  * Get the system wide closid/rmid limits. The number of closids should not be
  * affected by CDP.
  */
 u32 resctrl_arch_system_num_closid(void);
 u32 resctrl_arch_system_num_rmid(void);
+
+static inline void resctrl_arch_enable_alloc(void)
+{
+	static_branch_enable_cpuslocked(&rdt_alloc_enable_key);
+}
+
+static inline void resctrl_arch_disable_alloc(void)
+{
+	static_branch_disable_cpuslocked(&rdt_alloc_enable_key);
+}
+
+static inline void resctrl_arch_enable_mon(void)
+{
+	static_branch_enable_cpuslocked(&rdt_mon_enable_key);
+}
+
+static inline void resctrl_arch_disable_mon(void)
+{
+	static_branch_disable_cpuslocked(&rdt_mon_enable_key);
+}
 
 static inline u32 resctrl_arch_max_rmid_threshold(void)
 {
