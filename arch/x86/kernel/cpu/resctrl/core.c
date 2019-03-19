151400d64910ba06c4a8ccff885593a1c4975469
James Morse <james.morse@arm.com>
2019-03-19 16:50:04 +0000

    x86/resctrl: Add cpu online callback for resctrl work
    The resctrl architecture specific code may need to create a domain when
    a CPU comes online, it also needs to reset the CPUs PQR_ASSOC register.
    The resctrl filesystem code needs to update the rdtgroup_default cpu
    mask when cpus are brought online.
    
    Currently this is all done in one function, resctrl_online_cpu().
    This will need to be split into architecture and filesystem parts
    before resctrl can be moved to /fs/.
    
    Pull the rdtgroup_default update work out as a filesystem specific
    cpu_online helper. resctrl_online_cpu() is the obvious name for this,
    which means the version in core.c needs renaming.
    
    resctrl_online_cpu() is called by the arch code once it has done the
    work to add the new cpu to any domains.
    
    In future patches, resctrl_online_cpu() will take the rdtgroup_mutex
    itself.
    
    Tested-by: Shaopeng Tan <tan.shaopeng@fujitsu.com>
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -578,19 +578,20 @@ static void clear_closid_rmid(int cpu)
 	wrmsr(IA32_PQR_ASSOC, 0, 0);
 }
 
-static int resctrl_online_cpu(unsigned int cpu)
+static int resctrl_arch_online_cpu(unsigned int cpu)
 {
 	struct rdt_resource *r;
+	int err;
 
 	mutex_lock(&rdtgroup_mutex);
 	for_each_capable_rdt_resource(r)
 		domain_add_cpu(cpu, r);
-	/* The cpu is set in default rdtgroup after online. */
-	cpumask_set_cpu(cpu, &rdtgroup_default.cpu_mask);
 	clear_closid_rmid(cpu);
+
+	err = resctrl_online_cpu(cpu);
 	mutex_unlock(&rdtgroup_mutex);
 
-	return 0;
+	return err;
 }
 
 static void clear_childcpus(struct rdtgroup *r, unsigned int cpu)
@@ -919,7 +920,7 @@ static int __init resctrl_late_init(void)
 
 	state = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN,
 				  "x86/resctrl/cat:online:",
-				  resctrl_online_cpu, resctrl_offline_cpu);
+				  resctrl_arch_online_cpu, resctrl_offline_cpu);
 	if (state < 0)
 		return state;
 
