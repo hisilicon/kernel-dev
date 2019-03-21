9e2c1902cda7a03949446b0ee4b5b16d81ae6072
James Morse <james.morse@arm.com>
2019-03-21 16:32:19 +0000

    arm64: mpam: Add arch hooks for MPAM user
    Once MPAM has been succesfully probed by the driver and is in-use
    we want to enable its context-switching code. Provide a static-key
    and helpers to read/write it.
    
    Driver code will also need to read the sanitised id registers to
    find the common max partid/pmg values for all the online CPUs.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    
    The user of this comes later, adding it early should save the arch
    maintainer from spotting in the bigger mpam series.

--- arch/arm64/kernel/mpam.c
+++ arch/arm64/kernel/mpam.c
@@ -9,3 +9,19 @@
 DEFINE_STATIC_KEY_FALSE(arm64_system_has_mpam);
 DEFINE_STATIC_KEY_FALSE(arm64_mpam_has_hcr);
 DEFINE_PER_CPU(struct mpam_cpu_state, mpam_cpu_state);
+
+u16 mpam_cpu_max_partids(void)
+{
+	u64 idr;
+
+	idr = read_sanitised_ftr_reg(SYS_MPAMIDR_EL1);
+	return idr & MPAMIDR_PARTID_MASK;
+}
+
+u16 mpam_cpu_max_pmgs(void)
+{
+	u64 idr;
+
+	idr = read_sanitised_ftr_reg(SYS_MPAMIDR_EL1);
+	return (idr & MPAMIDR_PMG_MASK) >> MPAMIDR_PMG_SHIFT;
+}
