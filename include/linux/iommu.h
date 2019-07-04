4a508ef11cdf2dd185c9d6196962539a6094985b
Jacob Pan <jacob.jun.pan@linux.intel.com>
2019-07-04 19:13:28 +0100

    iommu: Introduce attach/detach_pasid_table API
    In virtualization use case, when a guest is assigned
    a PCI host device, protected by a virtual IOMMU on the guest,
    the physical IOMMU must be programmed to be consistent with
    the guest mappings. If the physical IOMMU supports two
    translation stages it makes sense to program guest mappings
    onto the first stage/level (ARM/Intel terminology) while the host
    owns the stage/level 2.
    
    In that case, it is mandated to trap on guest configuration
    settings and pass those to the physical iommu driver.
    
    This patch adds a new API to the iommu subsystem that allows
    to set/unset the pasid table information.
    
    A generic iommu_pasid_table_config struct is introduced in
    a new iommu.h uapi header. This is going to be used by the VFIO
    user API.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
    Signed-off-by: Liu, Yi L <yi.l.liu@linux.intel.com>
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -248,6 +248,8 @@ struct iommu_iotlb_gather {
  * @cache_invalidate: invalidate translation caches
  * @sva_bind_gpasid: bind guest pasid and mm
  * @sva_unbind_gpasid: unbind guest pasid and mm
+ * @attach_pasid_table: attach a pasid table
+ * @detach_pasid_table: detach the pasid table
  * @pgsize_bitmap: bitmap of all possible supported page sizes
  * @owner: Driver module providing these ops
  */
@@ -307,7 +309,6 @@ struct iommu_ops {
 				      void *drvdata);
 	void (*sva_unbind)(struct iommu_sva *handle);
 	int (*sva_get_pasid)(struct iommu_sva *handle);
-
 	int (*page_response)(struct device *dev,
 			     struct iommu_fault_event *evt,
 			     struct iommu_page_response *msg);
@@ -317,6 +318,9 @@ struct iommu_ops {
 			struct device *dev, struct iommu_gpasid_bind_data *data);
 
 	int (*sva_unbind_gpasid)(struct device *dev, int pasid);
+	int (*attach_pasid_table)(struct iommu_domain *domain,
+				  struct iommu_pasid_table_config *cfg);
+	void (*detach_pasid_table)(struct iommu_domain *domain);
 
 	unsigned long pgsize_bitmap;
 	struct module *owner;
@@ -456,6 +460,9 @@ extern int iommu_sva_bind_gpasid(struct iommu_domain *domain,
 		struct device *dev, struct iommu_gpasid_bind_data *data);
 extern int iommu_sva_unbind_gpasid(struct iommu_domain *domain,
 				struct device *dev, ioasid_t pasid);
+extern int iommu_attach_pasid_table(struct iommu_domain *domain,
+				    struct iommu_pasid_table_config *cfg);
+extern void iommu_detach_pasid_table(struct iommu_domain *domain);
 extern struct iommu_domain *iommu_get_domain_for_dev(struct device *dev);
 extern struct iommu_domain *iommu_get_dma_domain(struct device *dev);
 extern int iommu_map(struct iommu_domain *domain, unsigned long iova,
@@ -1083,6 +1090,16 @@ static inline int iommu_sva_unbind_gpasid(struct iommu_domain *domain,
 {
 	return -ENODEV;
 }
+
+static inline
+int iommu_attach_pasid_table(struct iommu_domain *domain,
+			     struct iommu_pasid_table_config *cfg)
+{
+	return -ENODEV;
+}
+
+static inline
+void iommu_detach_pasid_table(struct iommu_domain *domain) {}
 
 #endif /* CONFIG_IOMMU_API */
 
