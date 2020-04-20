d68f9cf959e3f79f6019eb1737fe01552fd8d12f
Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
2020-04-20 18:03:46 +0100

    Debug: Useful STE prints and report events in host as well.
    Signed-off-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>

--- drivers/iommu/arm-smmu-v3.c
+++ drivers/iommu/arm-smmu-v3.c
@@ -2300,6 +2300,9 @@ static void arm_smmu_write_strtab_ent(struct arm_smmu_master *master, u32 sid,
 
 	ste_live = s1_live || s2_live;
 
+	if (master)
+                dev_info(master->dev, "%s sid=%d from s1_live=%d s2_live=%d \n", __func__, sid, s1_live, s2_live);
+
 	/* Nuke the existing STE_0 value, as we're going to rewrite it */
 	val = STRTAB_STE_0_V;
 
@@ -2307,6 +2310,9 @@ static void arm_smmu_write_strtab_ent(struct arm_smmu_master *master, u32 sid,
 
 	abort = (!smmu_domain && disable_bypass) || smmu_domain->abort;
 	translate = s1_cfg || s2_cfg;
+
+	if (master)
+                dev_info(master->dev, "%s sid=%d abort=%d target=%d\n", __func__, sid, abort, translate);
 
 	if (abort || !translate) {
 		if (abort)
@@ -2322,8 +2328,11 @@ static void arm_smmu_write_strtab_ent(struct arm_smmu_master *master, u32 sid,
 		 * The SMMU can perform negative caching, so we must sync
 		 * the STE regardless of whether the old value was live.
 		 */
-		if (smmu)
+		if (smmu) {
+			if (master)
+                                dev_info(master->dev, "%s sid=%d invalidate ste with abort/bypass\n", __func__, sid);
 			arm_smmu_sync_ste_for_sid(smmu, sid);
+		}
 		return;
 	}
 
@@ -2334,6 +2343,8 @@ static void arm_smmu_write_strtab_ent(struct arm_smmu_master *master, u32 sid,
 	if (ste_live) {
 		/* First invalidate the live STE */
 		dst[0] = cpu_to_le64(STRTAB_STE_0_CFG_ABORT);
+		if (master)
+                        dev_info(master->dev, "%s sid=%d invalidate ste CFG_ABORT\n", __func__, sid);
 		arm_smmu_sync_ste_for_sid(smmu, sid);
 	}
 
@@ -2389,11 +2400,21 @@ static void arm_smmu_write_strtab_ent(struct arm_smmu_master *master, u32 sid,
 	if (master->ats_enabled)
 		dst[1] |= cpu_to_le64(FIELD_PREP(STRTAB_STE_1_EATS,
 						 STRTAB_STE_1_EATS_TRANS));
+	 if (master)
+                dev_info(master->dev, "%s sid=%d invalidate ste with s1=%d s2=%d\n",
+                        __func__, sid, !!s1_cfg, !!s2_cfg);
 
 	arm_smmu_sync_ste_for_sid(smmu, sid);
 	/* See comment in arm_smmu_write_ctx_desc() */
 	WRITE_ONCE(dst[0], cpu_to_le64(val));
 	arm_smmu_sync_ste_for_sid(smmu, sid);
+
+	if (master) {
+		dev_info(master->dev, "%s sid=%d ste dst[0] 0x%llx\n",__func__, sid, dst[0]);
+		dev_info(master->dev, "%s sid=%d ste dst[1] 0x%llx\n",__func__, sid, dst[1]);
+		dev_info(master->dev, "%s sid=%d ste dst[2] 0x%llx\n",__func__, sid, dst[2]);
+		dev_info(master->dev, "%s sid=%d ste dst[3] 0x%llx\n",__func__, sid, dst[3]);
+	}
 
 	/* It's likely that we'll want to use the new STE soon */
 	if (!(smmu->options & ARM_SMMU_OPT_SKIP_PREFETCH))
@@ -2644,8 +2665,8 @@ static irqreturn_t arm_smmu_evtq_thread(int irq, void *dev)
 				wake_up_all_locked(&q->wq);
 				num_handled = 0;
 			}
-
-			if (ret && __ratelimit(&rs)) {
+			/* Print evt recd anyway */
+			if (__ratelimit(&rs)) {
 				dev_info(smmu->dev, "event 0x%02x received:\n", id);
 				for (i = 0; i < ARRAY_SIZE(evt); ++i)
 					dev_info(smmu->dev, "\t0x%016llx\n",
