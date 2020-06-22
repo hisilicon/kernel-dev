5b7db9d32d927c203f23f2198914972f859cc81f
Zhangfei Gao <zhangfei.gao@linaro.org>
2020-06-22 14:10:29 +0800

    crypto: hisilicon - suppress ras error report
    When test with kill, too many ras error reported.
    Suppress the error report.
    
    Signed-off-by: Zhangfei Gao <zhangfei.gao@linaro.org>

--- drivers/crypto/hisilicon/zip/zip_main.c
+++ drivers/crypto/hisilicon/zip/zip_main.c
@@ -311,8 +311,11 @@ static void hisi_zip_hw_error_enable(struct hisi_qm *qm)
 	writel(HZIP_CORE_INT_RAS_NFE_ENABLE,
 		qm->io_base + HZIP_CORE_INT_RAS_NFE_ENB);
 
-	/* enable ZIP hw error interrupts */
-	writel(0, qm->io_base + HZIP_CORE_INT_MASK_REG);
+	if (qm->ver == QM_HW_V2)
+		/* ignore ras */
+		writel(0xc, qm->io_base + HZIP_CORE_INT_MASK_REG);
+	else
+		writel(0, qm->io_base + HZIP_CORE_INT_MASK_REG);
 
 	/* enable ZIP block master OOO when m-bit error occur */
 	val = readl(qm->io_base + HZIP_SOFT_CTRL_ZIP_CONTROL);
