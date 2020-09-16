6d80100653e9b80478a89da9255cb80eeb3a7561
Zengruan Ye <yezengruan@huawei.com>
2020-09-16 10:35:02 +0100

    KVM: arm64: Detect the ARMv8.6 TWED feature
    TWED is an optional feature introduced in ARMv8.6 Extensions.
    This feature provides a way to configure a delay in taking a
    trap of  WFE caused by HCR_EL2.TWE.
    
    Let us detect the feature to add support.
    
    Signed-off-by: Zengruan Ye <yezengruan@huawei.com>
    Signed-off-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -2121,6 +2121,18 @@ static const struct arm64_cpu_capabilities arm64_features[] = {
 		.sign = FTR_UNSIGNED,
 	},
 #endif
+#ifdef CONFIG_ARM64_TWED
+	{
+		.desc = "Delayed Trapping of WFE",
+		.capability = ARM64_HAS_TWED,
+		.type = ARM64_CPUCAP_SYSTEM_FEATURE,
+		.matches = has_cpuid_feature,
+		.sys_reg = SYS_ID_AA64MMFR1_EL1,
+		.field_pos = ID_AA64MMFR1_TWED_SHIFT,
+		.sign = FTR_UNSIGNED,
+		.min_field_value = 1,
+	},
+#endif
 	{},
 };
 
