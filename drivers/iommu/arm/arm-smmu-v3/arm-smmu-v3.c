c63624b9918cb267bc3e878ce4b739087e9cebf2
Eric Auger <eric.auger@redhat.com>
2020-11-02 14:47:02 +0100

    iommu/smmuv3: Disallow nested mode in presence of HW MSI regions
    Nested mode currently is not compatible with HW MSI reserved regions.
    Indeed MSI transactions targeting those MSI doorbells bypass the SMMU.
    This would require the guest to also bypass those ranges but the guest
    has no information about them.
    
    Let's check nested mode is not attempted in such configuration.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -2486,6 +2486,23 @@ static void arm_smmu_detach_dev(struct arm_smmu_master *master)
 	arm_smmu_install_ste_for_dev(master);
 }
 
+static bool arm_smmu_has_hw_msi_resv_region(struct device *dev)
+{
+	struct iommu_resv_region *region;
+	bool has_msi_resv_region = false;
+	LIST_HEAD(resv_regions);
+
+	iommu_get_resv_regions(dev, &resv_regions);
+	list_for_each_entry(region, &resv_regions, list) {
+		if (region->type == IOMMU_RESV_MSI) {
+			has_msi_resv_region = true;
+			break;
+		}
+	}
+	iommu_put_resv_regions(dev, &resv_regions);
+	return has_msi_resv_region;
+}
+
 static int arm_smmu_attach_dev(struct iommu_domain *domain, struct device *dev)
 {
 	int ret = 0;
@@ -2540,6 +2557,12 @@ static int arm_smmu_attach_dev(struct iommu_domain *domain, struct device *dev)
 		   smmu_domain->stall_enabled != master->stall_enabled) {
 		dev_err(dev, "cannot attach to stall-%s domain\n",
 			smmu_domain->stall_enabled ? "enabled" : "disabled");
+		ret = -EINVAL;
+		goto out_unlock;
+	}
+	/* Nested mode is not compatible with MSI HW reserved regions */
+	if (smmu_domain->stage == ARM_SMMU_DOMAIN_NESTED &&
+	    arm_smmu_has_hw_msi_resv_region(dev)) {
 		ret = -EINVAL;
 		goto out_unlock;
 	}
