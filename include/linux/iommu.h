8c239e4e1d51d0b2cfe15a5fc3a1860e2fd354e7
Jean-Philippe Brucker <jean-philippe@linaro.org>
2020-11-24 17:37:10 +0000

    of/iommu: Support dma-can-stall property
    Copy the dma-can-stall property into the fwspec structure.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -592,6 +592,7 @@ struct iommu_group *fsl_mc_device_group(struct device *dev);
  * @iommu_fwnode: firmware handle for this device's IOMMU
  * @iommu_priv: IOMMU driver private data for this device
  * @num_pasid_bits: number of PASID bits supported by this device
+ * @can_stall: the device is allowed to stall
  * @num_ids: number of associated device IDs
  * @ids: IDs which this device may present to the IOMMU
  */
@@ -600,6 +601,7 @@ struct iommu_fwspec {
 	struct fwnode_handle	*iommu_fwnode;
 	u32			flags;
 	u32			num_pasid_bits;
+	bool			can_stall;
 	unsigned int		num_ids;
 	u32			ids[];
 };
