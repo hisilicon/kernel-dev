9ad434901d675658e3e8499294e536d308c9cc89
John Garry <john.garry@huawei.com>
2021-02-11 12:33:12 +0000

    perf: refine some debug when changing cpuid

--- tools/perf/util/pmu.c
+++ tools/perf/util/pmu.c
@@ -703,10 +703,15 @@ struct pmu_events_map *perf_pmu__find_map(struct perf_pmu *pmu)
 	if (!cpuid)
 		return NULL;
 
-	pr_err("%s cpuid=%s pmu name=%s\n", __func__, cpuid, pmu->name);
-	if (!strcmp(pmu->name, "armv8_pmuv3_1"))
+//	pr_err("%s cpuid=%s pmu name=%s\n", __func__, cpuid, pmu->name);
+	if (!strcmp(pmu->name, "armv8_pmuv3_1")) {
+		static int count12;
+		if (count12 == 0)
+			pr_err("%s1 cpuid=0x00000000410fd030 pmu name=%s chnaged from %s\n", __func__, pmu->name, cpuid);
 		cpuid = (char *)"0x00000000410fd030";
-	pr_err("%s1 cpuid=%s pmu name=%s\n", __func__, cpuid, pmu->name);
+		count12++;
+	}
+//	pr_err("%s1 cpuid=%s pmu name=%s\n", __func__, cpuid, pmu->name);
 
 	i = 0;
 	for (;;) {
