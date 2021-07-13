535191a525c6a6bd039f9879544b63a0a679fa01
James Morse <james.morse@arm.com>
2021-07-13 18:48:23 +0100

    arm_mpam: resctrl: Add empty definitions for pseudo lock
    Pseudo lock isn't supported on arm64. Add empty definitions of the
    functions arm64 doesn't implement. Because the Kconfig option is not
    selected, none of these will be called.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- include/linux/arm_mpam.h
+++ include/linux/arm_mpam.h
@@ -76,4 +76,11 @@ struct rdt_resource;
 int resctrl_arch_mon_ctx_alloc_no_wait(struct rdt_resource *r, int evtid);
 void resctrl_arch_mon_ctx_free(struct rdt_resource *r, int evtid, int ctx);
 
+/* Pseudo lock is not supported by MPAM */
+static inline int resctrl_arch_pseudo_lock_fn(void *_plr) { return 0; }
+static inline int resctrl_arch_measure_l2_residency(void *_plr) { return 0; }
+static inline int resctrl_arch_measure_l3_residency(void *_plr) { return 0; }
+static inline int resctrl_arch_measure_cycles_lat_fn(void *_plr) { return 0; }
+static inline u64 resctrl_arch_get_prefetch_disable_bits(void) { return 0; }
+
 #endif /* __LINUX_ARM_MPAM_H */
