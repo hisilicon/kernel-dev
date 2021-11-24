1220b51fe951ceb37181f126041083e23c6f72e2
Jason Gunthorpe <jgg@nvidia.com>
2021-11-24 12:51:25 -0400

    interval-tree: Add a utility to iterate over spans in an interval tree
    The span iterator travels over the indexes of the interval_tree, not the
    nodes, and classifies spans of indexes as either 'used' or 'hole'.
    
    'used' spans are fully covered by nodes in the tree and 'hole' spans have
    no node intersecting the span.
    
    This is done greedily such that spans are maximally sized and every
    iteration step switches between used/hole.
    
    As an example a trivial allocator can be written as:
    
    	for (interval_tree_span_iter_first(&span, itree, 0, ULONG_MAX);
    	     !interval_tree_span_iter_done(&span);
    	     interval_tree_span_iter_next(&span))
    		if (span.is_hole &&
    		    span.last_hole - span.start_hole >= allocation_size - 1)
    			return span.start_hole;
    
    With all the tricky boundary conditions handled by the library code.
    
    The following iommufd patches have several algorithms for two of its
    overlapping node interval trees that are significantly simplified with
    this kind of iteration primitive. As it seems generally useful, put it
    into lib/.
    
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- include/linux/interval_tree.h
+++ include/linux/interval_tree.h
@@ -27,4 +27,45 @@ extern struct interval_tree_node *
 interval_tree_iter_next(struct interval_tree_node *node,
 			unsigned long start, unsigned long last);
 
+/*
+ * This iterator travels over spans in an interval tree. It does not return
+ * nodes but classifies each span as either a hole, where no nodes intersect, or
+ * a used, which is fully covered by nodes. Each iteration step toggles between
+ * hole and used until the entire range is covered. The returned spans always
+ * fully cover the requested range.
+ *
+ * The iterator is greedy, it always returns the largest hole or used possible,
+ * consolidating all consecutive nodes.
+ *
+ * Only is_hole, start_hole/used and last_hole/used are part of the external
+ * interface.
+ */
+struct interval_tree_span_iter {
+	struct interval_tree_node *nodes[2];
+	unsigned long first_index;
+	unsigned long last_index;
+	union {
+		unsigned long start_hole;
+		unsigned long start_used;
+	};
+	union {
+		unsigned long last_hole;
+		unsigned long last_used;
+	};
+	/* 0 == used, 1 == is_hole, -1 == done iteration */
+	int is_hole;
+};
+
+void interval_tree_span_iter_first(struct interval_tree_span_iter *state,
+				   struct rb_root_cached *itree,
+				   unsigned long first_index,
+				   unsigned long last_index);
+void interval_tree_span_iter_next(struct interval_tree_span_iter *state);
+
+static inline bool
+interval_tree_span_iter_done(struct interval_tree_span_iter *state)
+{
+	return state->is_hole == -1;
+}
+
 #endif	/* _LINUX_INTERVAL_TREE_H */
