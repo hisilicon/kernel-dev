53f1e618c429afcb99fb9f857c58c65d6fa242a3
John Garry <john.garry@huawei.com>
2021-11-25 14:11:09 +0000

    pseudo nmi

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -1842,7 +1842,7 @@ static void cpu_enable_e0pd(struct arm64_cpu_capabilities const *cap)
 #endif /* CONFIG_ARM64_E0PD */
 
 #ifdef CONFIG_ARM64_PSEUDO_NMI
-static bool enable_pseudo_nmi;
+static bool enable_pseudo_nmi = true;
 
 static int __init early_enable_pseudo_nmi(char *p)
 {
