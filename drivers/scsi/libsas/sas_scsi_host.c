a134d8068c072085e1a61fd43f6b1dc38a00b42e
John Garry <john.garry@huawei.com>
2022-01-24 18:17:15 +0000

    scsi: libsas: Add sas_execute_ata_cmd()
    Add a function to execute an ATA command using the TMF code, and use in
    the hisi_sas driver. That driver needs to be able to issue the command on
    a specific phy, so add an interface for that.
    
    With that, hisi_sas_exec_internal_tmf_task() may be deleted.
    
    Signed-off-by: John Garry <john.garry@huawei.com>

--- drivers/scsi/libsas/sas_scsi_host.c
+++ drivers/scsi/libsas/sas_scsi_host.c
@@ -939,6 +939,12 @@ int sas_execute_tmf(struct domain_device *device, void *parameter,
 		task->task_proto = device->tproto;
 
 		if (dev_is_sata(device)) {
+			task->ata_task.device_control_reg_update = 1;
+			if (force_phy_id >= 0) {
+				task->ata_task.force_phy = true;
+				task->ata_task.force_phy_id = force_phy_id;
+			}
+			memcpy(&task->ata_task.fis, parameter, para_len);
 		} else {
 			memcpy(&task->ssp_task, parameter, para_len);
 		}
