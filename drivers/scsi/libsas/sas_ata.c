b162adbab157227db142b0177a3bc1bd6907ed2b
John Garry <john.garry@huawei.com>
2022-03-07 15:19:33 +0000

    scsi: libsas: Refactor sas_ata_hard_reset()
    Create function sas_ata_wait_after_reset() from sas_ata_hard_reset() as
    some LLDDs may want to check for a remote ATA phy is up after reset.
    
    Signed-off-by: John Garry <john.garry@huawei.com>

--- drivers/scsi/libsas/sas_ata.c
+++ drivers/scsi/libsas/sas_ata.c
@@ -366,22 +366,13 @@ static int sas_ata_printk(const char *level, const struct domain_device *ddev,
 	return r;
 }
 
-static int sas_ata_hard_reset(struct ata_link *link, unsigned int *class,
-			      unsigned long deadline)
+int sas_ata_wait_after_reset(struct ata_link *link, unsigned long deadline)
 {
-	int ret = 0, res;
-	struct sas_phy *phy;
 	struct ata_port *ap = link->ap;
-	int (*check_ready)(struct ata_link *link);
 	struct domain_device *dev = ap->private_data;
-	struct sas_internal *i = dev_to_sas_internal(dev);
-
-	res = i->dft->lldd_I_T_nexus_reset(dev);
-	if (res == -ENODEV)
-		return res;
-
-	if (res != TMF_RESP_FUNC_COMPLETE)
-		sas_ata_printk(KERN_DEBUG, dev, "Unable to reset ata device?\n");
+	int (*check_ready)(struct ata_link *link);
+	struct sas_phy *phy;
+	int ret;
 
 	phy = sas_get_local_phy(dev);
 	if (scsi_is_sas_phy_local(phy))
@@ -393,6 +384,27 @@ static int sas_ata_hard_reset(struct ata_link *link, unsigned int *class,
 	ret = ata_wait_after_reset(link, deadline, check_ready);
 	if (ret && ret != -EAGAIN)
 		sas_ata_printk(KERN_ERR, dev, "reset failed (errno=%d)\n", ret);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(sas_ata_wait_after_reset);
+
+static int sas_ata_hard_reset(struct ata_link *link, unsigned int *class,
+			      unsigned long deadline)
+{
+	struct ata_port *ap = link->ap;
+	struct domain_device *dev = ap->private_data;
+	struct sas_internal *i = dev_to_sas_internal(dev);
+	int ret;
+
+	ret = i->dft->lldd_I_T_nexus_reset(dev);
+	if (ret == -ENODEV)
+		return ret;
+
+	if (ret != TMF_RESP_FUNC_COMPLETE)
+		sas_ata_printk(KERN_DEBUG, dev, "Unable to reset ata device?\n");
+
+	ret = sas_ata_wait_after_reset(link, deadline);
 
 	*class = dev->sata_dev.class;
 
