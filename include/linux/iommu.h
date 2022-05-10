c8f112145ebba07aafea9473383cee29cb1bf6af
Jason Gunthorpe <jgg@nvidia.com>
2022-05-10 13:55:24 -0300

    vfio: Remove VFIO_TYPE1_NESTING_IOMMU
    This control causes the ARM SMMU drivers to choose a stage 2
    implementation for the IO pagetable (vs the stage 1 usual default),
    however this choice has no visible impact to the VFIO user. Further qemu
    never implemented this and no other userspace user is known.
    
    The original description in commit f5c9ecebaf2a ("vfio/iommu_type1: add
    new VFIO_TYPE1_NESTING_IOMMU IOMMU type") suggested this was to "provide
    SMMU translation services to the guest operating system" however the rest
    of the API to set the guest table pointer for the stage 1 was never
    completed, or at least never upstreamed, rendering this part useless dead
    code.
    
    Since the current patches to enable nested translation, aka userspace page
    tables, rely on iommufd and will not use the enable_nesting()
    iommu_domain_op, remove this infrastructure. However, don't cut too deep
    into the SMMU drivers for now expecting the iommufd work to pick it up -
    we still need to create S2 IO page tables.
    
    Remove VFIO_TYPE1_NESTING_IOMMU and everything under it including the
    enable_nesting iommu_domain_op.
    
    Just in-case there is some userspace using this continue to treat
    requesting it as a NOP, but do not advertise support any more.
    
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -334,7 +334,6 @@ struct iommu_ops {
  * @enforce_cache_coherency: Prevent any kind of DMA from bypassing IOMMU_CACHE,
  *                           including no-snoop TLPs on PCIe or other platform
  *                           specific mechanisms.
- * @enable_nesting: Enable nesting
  * @set_pgtable_quirks: Set io page table quirks (IO_PGTABLE_QUIRK_*)
  * @free: Release the domain after use.
  */
@@ -366,7 +365,6 @@ struct iommu_domain_ops {
 				    dma_addr_t iova);
 
 	bool (*enforce_cache_coherency)(struct iommu_domain *domain);
-	int (*enable_nesting)(struct iommu_domain *domain);
 	int (*set_pgtable_quirks)(struct iommu_domain *domain,
 				  unsigned long quirks);
 
@@ -540,7 +538,6 @@ extern int iommu_page_response(struct device *dev,
 extern int iommu_group_id(struct iommu_group *group);
 extern struct iommu_domain *iommu_group_default_domain(struct iommu_group *);
 
-int iommu_enable_nesting(struct iommu_domain *domain);
 int iommu_set_pgtable_quirks(struct iommu_domain *domain,
 		unsigned long quirks);
 
