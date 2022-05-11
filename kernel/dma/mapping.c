9a5389fde0cc6f5978684f2d195777f9c1ca79ac
John Garry <john.garry@huawei.com>
2022-05-11 14:33:39 +0100

    dma_max_mapping_size print

--- kernel/dma/mapping.c
+++ kernel/dma/mapping.c
@@ -764,11 +764,14 @@ size_t dma_max_mapping_size(struct device *dev)
 	const struct dma_map_ops *ops = get_dma_ops(dev);
 	size_t size = SIZE_MAX;
 
+	dev_err(dev, "%s ops=%pS ops->max_mapping_size=%pS\n", __func__, ops, ops ? ops->max_mapping_size : NULL);
 	if (dma_map_direct(dev, ops))
 		size = dma_direct_max_mapping_size(dev);
 	else if (ops && ops->max_mapping_size)
 		size = ops->max_mapping_size(dev);
 
+	dev_err(dev, "%s2 ops=%pS ops->max_mapping_size=%pS size=%zu\n", 
+		__func__, ops, ops ? ops->max_mapping_size : NULL, size);
 	return size;
 }
 EXPORT_SYMBOL_GPL(dma_max_mapping_size);
