1066ab3be463a1e3431dbb66baa797736bca825d
John Garry <john.garry@huawei.com>
2022-06-07 14:20:11 +0100

    scsi: libsas drivers: Prepare for reserved commands
    Set the various LLDD fields to be able to use reserved commands. I have
    only done this for hisi sas v2 HW, but all others need this treatment.
    
    Signed-off-by: John Garry <john.garry@huawei.com>

--- drivers/scsi/hisi_sas/hisi_sas_v2_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v2_hw.c
@@ -3562,6 +3562,7 @@ static struct scsi_host_template sht_v2_hw = {
 	.proc_name		= DRV_NAME,
 	.module			= THIS_MODULE,
 	.queuecommand		= sas_queuecommand,
+	.reserved_queuecommand = sas_queuecommand_internal,
 	.dma_need_drain		= ata_scsi_dma_need_drain,
 	.target_alloc		= sas_target_alloc,
 	.slave_configure	= hisi_sas_slave_configure,
