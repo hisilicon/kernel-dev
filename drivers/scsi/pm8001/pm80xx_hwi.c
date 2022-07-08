dd26e47164b20248e3f8950467025201293e039e
John Garry <john.garry@huawei.com>
2022-07-08 11:35:55 +0100

    IO_XFER_ERROR_ABORTED_NCQ_MODE

--- drivers/scsi/pm8001/pm80xx_hwi.c
+++ drivers/scsi/pm8001/pm80xx_hwi.c
@@ -2797,6 +2797,7 @@ static void mpi_sata_event(struct pm8001_hba_info *pm8001_ha,
 
 	/* Check if this is NCQ error */
 	if (event == IO_XFER_ERROR_ABORTED_NCQ_MODE) {
+		pr_err("%s IO_XFER_ERROR_ABORTED_NCQ_MODE\n", __func__);
 		/* find device using device id */
 		pm8001_dev = pm8001_find_dev(pm8001_ha, dev_id);
 		/* send read log extension */
