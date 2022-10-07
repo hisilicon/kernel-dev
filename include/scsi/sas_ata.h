a34b8c5b9056af8a1f8d992d5a7773e0ff49e0e1
John Garry <john.garry@huawei.com>
2022-10-07 12:03:42 +0100

    remove dev_is_sata dev == NULL check

--- include/scsi/sas_ata.h
+++ include/scsi/sas_ata.h
@@ -17,9 +17,6 @@
 
 static inline int dev_is_sata(struct domain_device *dev)
 {
-	if (!dev)
-		return false;
-
 	return dev->dev_type == SAS_SATA_DEV || dev->dev_type == SAS_SATA_PM ||
 	       dev->dev_type == SAS_SATA_PM_PORT || dev->dev_type == SAS_SATA_PENDING;
 }
