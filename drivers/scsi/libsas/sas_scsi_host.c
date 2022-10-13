1edf2d0e78ab72b104fa393e6ddd19fda897b0cc
John Garry <john.garry@huawei.com>
2022-10-13 12:36:47 +0100

    scsi: libsas: Queue TMF commands as requests
    Like what we did with SMP commands, send TMF commands through the block
    layer.
    
    In future we can now also take advantage of the block layer request
    timeout handling.
    
    Signed-off-by: John Garry <john.garry@huawei.com>

--- drivers/scsi/libsas/sas_scsi_host.c
+++ drivers/scsi/libsas/sas_scsi_host.c
@@ -1065,12 +1065,17 @@ int sas_execute_tmf(struct domain_device *device, void *parameter,
 	int res, retry;
 
 	for (retry = 0; retry < TASK_RETRY; retry++) {
-		task = sas_alloc_slow_task(GFP_KERNEL);
+		struct request *rq;
+
+		task = sas_alloc_slow_task_rq(device, GFP_KERNEL);
 		if (!task)
 			return -ENOMEM;
 
 		task->dev = device;
 		task->task_proto = device->tproto;
+
+		rq = scsi_cmd_to_rq(task->uldd_task);
+		rq->timeout = TASK_TIMEOUT;
 
 		if (dev_is_sata(device)) {
 			task->ata_task.device_control_reg_update = 1;
@@ -1083,20 +1088,10 @@ int sas_execute_tmf(struct domain_device *device, void *parameter,
 			memcpy(&task->ssp_task, parameter, para_len);
 		}
 
-		task->task_done = sas_task_internal_done;
+		task->task_done = sas_task_complete_internal;
 		task->tmf = tmf;
 
-		task->slow_task->timer.function = sas_task_internal_timedout;
-		task->slow_task->timer.expires = jiffies + TASK_TIMEOUT;
-		add_timer(&task->slow_task->timer);
-
-		res = i->dft->lldd_execute_task(task, GFP_KERNEL);
-		if (res) {
-			del_timer_sync(&task->slow_task->timer);
-			pr_err("executing TMF task failed %016llx (%d)\n",
-			       SAS_ADDR(device->sas_addr), res);
-			break;
-		}
+		blk_execute_rq_nowait(rq, true);
 
 		wait_for_completion(&task->slow_task->completion);
 
