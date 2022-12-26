c70713e8f8c66644919dbe267b4f60275f6d6311
Lu Baolu <baolu.lu@linux.intel.com>
2022-12-26 10:55:05 +0800

    iommu: Add new iommu op to create domains owned by userspace
    Introduce a new iommu_domain op to create domains owned by userspace,
    e.g. through iommufd. These domains have a few different properties
    compares to kernel owned domains:
    
     - They may be UNMANAGED domains, but created with special parameters.
       For instance aperture size changes/number of levels, different
       IOPTE formats, or other things necessary to make a vIOMMU work
    
     - We have to track all the memory allocations with GFP_KERNEL_ACCOUNT
       to make the cgroup sandbox stronger
    
     - Device-specialty domains, such as NESTED domains can be created by
       iommufd.
    
    The new op clearly says the domain is being created by IOMMUFD, that
    the domain is intended for userspace use, and it provides a way to pass
    a driver specific uAPI structure to customize the created domain to
    exactly what the vIOMMU userspace driver requires.
    
    iommu drivers that cannot support VFIO/IOMMUFD should not support this
    op. This includes any driver that cannot provide a fully functional
    UNMANAGED domain.
    
    This op chooses to make the special parameters opaque to the core. This
    suits the current usage model where accessing any of the IOMMU device
    special parameters does require a userspace driver that matches the
    kernel driver. If a need for common parameters, implemented similarly
    by several drivers, arises then there is room in the design to grow a
    generic parameter set as well.
    
    This new op for now is only supposed to be used by iommufd, hence no
    wrapper for it. iommufd would call the callback directly. As for domain
    free, iommufd would use iommu_domain_free().
    
    Also, add a pre-defined length of the input data structure that must be
    added to include/uapi/include/iommufd.h file. This helps the iommufd
    core to sanitize the input data before it forwards the data to an iommu
    driver.
    
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Co-developed-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -229,6 +229,10 @@ struct iommu_iotlb_gather {
  *           after use. Return the data buffer if success, or ERR_PTR on
  *           failure.
  * @domain_alloc: allocate iommu domain
+ * @domain_alloc_user: allocate user iommu domain
+ * @domain_alloc_user_data_len: Defined length of user data to allocate a user
+ *                              iommu domain, being sizeof the data structure in
+ *                              include/uapi/linux/iommufd.h
  * @probe_device: Add device to iommu driver handling
  * @release_device: Remove device from iommu driver handling
  * @probe_finalize: Do final setup work after the device is added to an IOMMU
@@ -269,6 +273,10 @@ struct iommu_ops {
 
 	/* Domain allocation and freeing by the iommu driver */
 	struct iommu_domain *(*domain_alloc)(unsigned iommu_domain_type);
+	struct iommu_domain *(*domain_alloc_user)(struct device *dev,
+						  struct iommu_domain *parent,
+						  const void *user_data);
+	const size_t *domain_alloc_user_data_len;
 
 	struct iommu_device *(*probe_device)(struct device *dev);
 	void (*release_device)(struct device *dev);
