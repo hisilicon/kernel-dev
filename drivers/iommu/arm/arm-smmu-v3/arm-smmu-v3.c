df789012295f06b20cc22fbd5e99ba867cf8b6c5
Zhangfei Gao <zhangfei.gao@linaro.org>
2023-05-14 14:19:56 +0000

    iommu/arm-smmu-v3: Temp hack for vSVA
    This is a quick fix to get the vSVA working on HiSilicon platform.
    Requires proper fix.
    
    Issue: Guest reports s1cdmax = 0xa while master ssid is 0x10
    
    Signed-off-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
    
    Signed-off-by: Zhangfei Gao <zhangfei.gao@linaro.org>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -2269,6 +2269,7 @@ arm_smmu_domain_finalise_nested(struct iommu_domain *domain,
 	smmu_domain->s1_cfg.s1fmt = s1fmt;
 	smmu_domain->s1_cfg.s1cdmax = s1cdmax;
 	smmu_domain->s1_cfg.cdcfg.cdtab_dma = s1ctxptr;
+	smmu_domain->stall_enabled = master->stall_enabled;
 	return 0;
 }
 
@@ -2555,7 +2556,7 @@ static int arm_smmu_attach_dev(struct iommu_domain *domain, struct device *dev)
 		ret = -EINVAL;
 		goto out_unlock;
 	} else if (smmu_domain->stage == ARM_SMMU_DOMAIN_S1 &&
-		   master->ssid_bits != smmu_domain->s1_cfg.s1cdmax) {
+		   master->ssid_bits < smmu_domain->s1_cfg.s1cdmax) {
 		ret = -EINVAL;
 		goto out_unlock;
 	} else if (smmu_domain->stage == ARM_SMMU_DOMAIN_S1 &&
