d656e9e199bb022f53a4916656b7b63015197887
Yi Liu <yi.l.liu@intel.com>
2023-06-08 01:02:48 -0700

    iommufd: Add data structure for Intel VT-d stage-1 cache invalidation
    This adds the data structure for flushing iotlb for the nested domain
    allocated with IOMMU_HWPT_DATA_VTD_S1 type.
    
    This only supports invalidating IOTLB, but no for device-TLB as device-TLB
    invalidation will be covered automatically in the IOTLB invalidation if the
    underlying IOMMU driver has enabled ATS for the affected device.
    
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- include/uapi/linux/iommufd.h
+++ include/uapi/linux/iommufd.h
@@ -615,6 +615,42 @@ struct iommu_hwpt_get_dirty_bitmap {
 					IOMMUFD_CMD_HWPT_GET_DIRTY_BITMAP)
 
 /**
+ * enum iommu_hwpt_vtd_s1_invalidate_flags - Flags for Intel VT-d
+ *                                           stage-1 cache invalidation
+ * @IOMMU_VTD_INV_FLAGS_LEAF: The LEAF flag indicates whether only the
+ *                            leaf PTE caching needs to be invalidated
+ *                            and other paging structure caches can be
+ *                            preserved.
+ */
+enum iommu_hwpt_vtd_s1_invalidate_flags {
+	IOMMU_VTD_INV_FLAGS_LEAF = 1 << 0,
+};
+
+/**
+ * struct iommu_hwpt_vtd_s1_invalidate - Intel VT-d cache invalidation
+ *                                       (IOMMU_HWPT_DATA_VTD_S1)
+ * @addr: The start address of the addresses to be invalidated. It needs
+ *        to be 4KB aligned.
+ * @npages: Number of contiguous 4K pages to be invalidated.
+ * @flags: Combination of enum iommu_hwpt_vtd_s1_invalidate_flags
+ * @__reserved: Must be 0
+ *
+ * The Intel VT-d specific invalidation data for user-managed stage-1 cache
+ * invalidation in nested translation. Userspace uses this structure to
+ * tell the impacted cache scope after modifying the stage-1 page table.
+ *
+ * Invalidating all the caches related to the page table by setting @addr
+ * to be 0 and @npages to be __aligned_u64(-1). This includes the
+ * corresponding device-TLB if ATS is enabled on the attached devices.
+ */
+struct iommu_hwpt_vtd_s1_invalidate {
+	__aligned_u64 addr;
+	__aligned_u64 npages;
+	__u32 flags;
+	__u32 __reserved;
+};
+
+/**
  * struct iommu_hwpt_invalidate - ioctl(IOMMU_HWPT_INVALIDATE)
  * @size: sizeof(struct iommu_hwpt_invalidate)
  * @hwpt_id: HWPT ID of a nested HWPT for cache invalidation
